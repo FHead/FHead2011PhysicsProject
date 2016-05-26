95187c75e92279e415e85ce4b82bbb8fff1a4a1a
Yi <chen.yi.first@gmail.com>
2016-05-26 02:31:32 -0700

    Likelihood

---
+++ HiggsProperties/Likelihood/15123_AttachFullEFTLikelihoodZAAZWithPDF/MakeBranchCentralSignal.sh
@@ -0,0 +1,39 @@
+for i in \
+   A1ZZR A1ZZI \
+   A2ZZR A2ZZI \
+   A3ZZR A3ZZI \
+   A4ZZR A4ZZI \
+   A1ZAR A1ZAI \
+   A2ZAR A2ZAI \
+   A3ZAR A3ZAI \
+   A4ZAR A4ZAI \
+   A1AZR A1AZI \
+   A2AZR A2AZI \
+   A3AZR A3AZI \
+   A4AZR A4AZI \
+   A1AAR A1AAI \
+   A2AAR A2AAI \
+   A3AAR A3AAI \
+   A4AAR A4AAI
+do
+   for j in \
+      A1ZZR A1ZZI \
+      A2ZZR A2ZZI \
+      A3ZZR A3ZZI \
+      A4ZZR A4ZZI \
+      A1ZAR A1ZAI \
+      A2ZAR A2ZAI \
+      A3ZAR A3ZAI \
+      A4ZAR A4ZAI \
+      A1AZR A1AZI \
+      A2AZR A2AZI \
+      A3AZR A3AZI \
+      A4AZR A4AZI \
+      A1AAR A1AAI \
+      A2AAR A2AAI \
+      A3AAR A3AAI \
+      A4AAR A4AAI
+   do
+      echo B1_${i}_${j}
+   done
+done
---
+++ HiggsProperties/Likelihood/15253_AttachFullEFTLikelihoodZAAZWithPDF/CondorSubmit/RunLikelihood.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /home/yichen/CMSSW/CMSSW_7_1_1/src
+eval `scramv1 runtime -sh`
+cd - 1>/dev/null 2>/dev/null
+
+echo $HOSTNAME
+echo "Current PWD = `pwd`"
+cat /proc/sys/kernel/hostname
+
+BaseDir=/home/yichen/PhysicsWorkspace/HiggsProperties/Likelihood/15253_AttachFullEFTLikelihoodZAAZWithPDF/
+WorkDir=`pwd`
+
+echo "Work directory = $WorkDir"
+
+touch $WorkDir/DummyFile
+
+ln -s $BaseDir/*cpp $WorkDir/
+ln -s $BaseDir/*h $WorkDir/
+ln -s $BaseDir/*dat $WorkDir/
+ln -s $BaseDir/OriginalTrees* $WorkDir/
+ln -s $BaseDir/AttachedTrees* $WorkDir/
+ln -s $BaseDir/BranchCentral* $WorkDir/
+
+sh compile.sh
+
+echo "Compilation ended, now the heavy I/O from/to hadoop starts"
+
+Input=$1
+Output=$2
+Seed=$3
+Energy=$4
+
+cp $1 Current.root
+
+./RunSignal $1 0 99999999 $Seed $Energy | cut --delim=' ' --field=3- > LogS
+head -5 LogS
+./AttachBranch BranchCentralSignal LogS Current.root
+# rm LogS
+
+./RunBackground $1 0 9999999 $Seed $Energy | cut --delim=' ' --field=3- > LogB
+head -5 LogB
+./AttachBranch BranchCentralBackground LogB Current.root
+# rm LogB
+
+mv Current.root $2
+
+
+

abeff0943e1634ecc53cb89439010d1ce581750c
Yi <chen.yi.first@gmail.com>
2017-02-16 11:40:10 +0100

    Modified PF Spectrum code

---
+++ HIJetMass/JetStudies/16133_ModifiedPFSpectrum/compile-local.sh
@@ -0,0 +1,4 @@
+CompileRootMacro JetMassPlots.cpp a.out Class.o \
+   -I../../CommonCode/include ../../CommonCode/library/* \
+   `fastjet-config --cxxflags` `fastjet-config --libs` \
+   /Users/yichen/Programs/fastjet/fastjet-3.2.1/../install321/lib/*.a

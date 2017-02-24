12f2f5abe9058ca175817cf4461c22bec942997f
Yi Chen <chen.yi.first@gmail.com>
2017-02-24 13:32:34 +0100

    15721 ZPrime workspace

---
+++ HiggsProperties/MELevelStudy/15721_ZPrimeWorkspace/CollectAll.sh
@@ -0,0 +1,36 @@
+cat Cleaned/Function_EEEE_*R_*R > CalculateME_Part31.cpp
+cat Cleaned/Function_EEEE_*R_*I > CalculateME_Part32.cpp
+cat Cleaned/Function_EEEE_*I_*R > CalculateME_Part33.cpp
+cat Cleaned/Function_EEEE_*I_*I > CalculateME_Part34.cpp
+cat Cleaned/Function_EEMM_*R_*R > CalculateME_Part35.cpp
+cat Cleaned/Function_EEMM_*R_*I > CalculateME_Part36.cpp
+cat Cleaned/Function_EEMM_*I_*R > CalculateME_Part37.cpp
+cat Cleaned/Function_EEMM_*I_*I > CalculateME_Part38.cpp
+
+for i in EEMM EEEE
+do
+   grep $i Header | grep -v I_ > ${i}RR
+   grep $i Header | grep -v R_ > ${i}II
+   grep $i Header | grep R_A | grep I_1 > ${i}RI
+   grep $i Header | grep I_A | grep R_1 > ${i}IR
+done
+
+cp ../../CommonCode/source/MEFunctions.cpp Meow0
+
+for i in EEMM EEEE
+do
+   for j in RR II RI IR
+   do
+      cat Meow0 > Meow1
+      echo "double CalculateXVV_${i}_${j}_15721(LeptonVectors &Leptons, ExtendedAVVBasis &A, ZPrimeParameters &P)" >> Meow1
+      echo "{" >> Meow1
+      echo "   return" >> Meow1
+      cat ${i}${j} >> Meow1
+      echo "}" >> Meow1
+      echo "" >> Meow1
+      mv Meow1 Meow0
+   done
+done
+
+
+

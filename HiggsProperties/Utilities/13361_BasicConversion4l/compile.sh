792e378e3af1be611f48684908c2b48f4fd13b9d
Yi Chen <chen.yi.first@gmail.com>
2017-02-24 13:45:04 +0100

    Meow

--- HiggsProperties/Utilities/13361_BasicConversion4l/compile.sh
+++ HiggsProperties/Utilities/13361_BasicConversion4l/compile.sh
@@ -2,12 +2,12 @@ g++ ConvertVectorsToAnglesRoberto.cpp -o ConvertVectorsToAnglesRoberto \
    -I$HOME/PhysicsWorkspace/HiggsProperties/CommonCode/include \
    -I$RootMacrosBase \
    $HOME/PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
-   $RootMacrosBase/Code/library/TauHelperFunctions2.o
+   $RootMacrosBase/Code/library/TauHelperFunctions3.o
 
 g++ ConvertXYZToVectors.cpp -o ConvertXYZToVectors \
    -I$HOME/PhysicsWorkspace/HiggsProperties/CommonCode/include \
    -I$RootMacrosBase \
    $HOME/PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
-   $RootMacrosBase/Code/library/TauHelperFunctions2.o
+   $RootMacrosBase/Code/library/TauHelperFunctions3.o
 
 

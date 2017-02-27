8d1bf2e0248ac4556b847db97c554b4317128d37
Yi Chen <chen.yi.first@gmail.com>
2017-02-27 17:14:42 +0100

    Merged evaluate likelihood

--- HiggsProperties/LightBosonStudies/16145_HypothesisTestingWithBackground/EvaluateLikelihood.cpp
+++ HiggsProperties/LightBosonStudies/16145_HypothesisTestingWithBackground/EvaluateLikelihood.cpp
@@ -501,6 +501,7 @@ int main(int argc, char *argv[])
 vector<Likelihood> ReadTree(string FileName, char Cut, bool IsEM)
 {
    vector<Likelihood> Result;
+   Result.reserve(100000);
 
    TFile F(FileName.c_str());
 

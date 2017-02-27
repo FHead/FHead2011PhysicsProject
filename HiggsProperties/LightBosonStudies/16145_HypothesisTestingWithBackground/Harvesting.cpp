1e60bc06494a691b57ae835071cdc99b76a6e167
Yi Chen <chen.yi.first@gmail.com>
2017-02-27 13:49:18 +0100

    Better version of hypothesis testing code

--- HiggsProperties/LightBosonStudies/16145_HypothesisTestingWithBackground/Harvesting.cpp
+++ HiggsProperties/LightBosonStudies/16145_HypothesisTestingWithBackground/Harvesting.cpp
@@ -19,6 +19,9 @@ int main(int argc, char *argv[]);
 vector<vector<double>> ReadLikelihood(string FileName);
 Triplet GetTriplet(int x, int N);
 ostream &operator <<(ostream &out, Triplet &T);
+pair<double, double> BinomialRange(int x, int N);
+double JntBinomial(int x, int N, double r);
+double LogFactorial(int N);
 
 struct Triplet
 {
@@ -34,8 +37,8 @@ int main(int argc, char *argv[])
    PdfFileHelper PdfFile("ResultDoubleFile_A1UU_A1UUpA3UU_F.pdf");
    PdfFile.AddTextPage("A1UU (black) vs A1UU+A3UU (red)");
 
-   string File1 = "LikelihoodFixed_A1UU_F_1_Scenario0";
-   string File2 = "LikelihoodFixed_A1UUpA3UU_F_1_Scenario0";
+   string File1 = "LikelihoodFixed_A1UU_F_1_Scenario1";
+   string File2 = "LikelihoodFixed_A1UUpA3UU_F_1_Scenario1";
 
    vector<vector<double>> L1 = ReadLikelihood(File1);
    vector<vector<double>> L2 = ReadLikelihood(File2);
@@ -112,7 +115,7 @@ int main(int argc, char *argv[])
             if(Max < L1[i][iM1] - L1[i][iM2])
                Max = L1[i][iM1] - L1[i][iM2];
             
-            L1D.push_back(L1[i][iM1] - L1[i][iM2]);
+            L1D[i] = L1[i][iM1] - L1[i][iM2];
          }
          for(int i = 0; i < (int)L2.size(); i++)
          {
@@ -121,7 +124,7 @@ int main(int argc, char *argv[])
             if(Max < L2[i][iM1] - L2[i][iM2])
                Max = L2[i][iM1] - L2[i][iM2];
 
-            L2D.push_back(L2[i][iM1] - L2[i][iM2]);
+            L2D[i] = L2[i][iM1] - L2[i][iM2];
          }
 
          double Range = Max - Min;
@@ -137,6 +140,10 @@ int main(int argc, char *argv[])
             HM1.Fill(L1[i][iM1] - L1[i][iM2]);
          for(int i = 0; i < (int)L2.size(); i++)
             HM2.Fill(L2[i][iM1] - L2[i][iM2]);
+         // for(int i = 0; i < (int)L1D.size(); i++)
+         //    HM1.Fill(L1D[i]);
+         // for(int i = 0; i < (int)L2D.size(); i++)
+         //    HM2.Fill(L2D[i]);
 
          HM1.SetStats(0);
          HM1.SetLineColor(kBlack);
@@ -205,7 +212,59 @@ int main(int argc, char *argv[])
             ModelP = 0;
          else  // There is some overlap
          {
-            ModelP = 0.5;
+            int BestI1 = -1;
+            int BestI2 = -1;
+            double BestD = -1;
+
+            bool Swapped = false;
+            if(L1D.size() < L2D.size())
+            {
+               swap(L1D, L2D);
+               Swapped = true;
+            }
+
+            for(int I1 = 0; I1 < (int)L1D.size(); I1++)
+            {
+               int I2Min = (int)((1 - (double)(I1 + 2) / L1D.size()) * L2D.size());
+               int I2Max = (int)((1 - (double)(I1 - 2) / L1D.size()) * L2D.size());
+
+               if(I2Min < 0)                  I2Min = 0;
+               if(I2Max >= (int)L2D.size())   I2Max = (int)L2D.size() - 1;
+
+               int BestI2R = I2Min;
+               double BestRD = fabs((double)I1 / (int)L1D.size() - (1 - (double)BestI2R / (int)L2D.size()));
+               
+               for(int I2 = I2Min; I2 < I2Max; I2++)
+               {
+                  double D = fabs((double)I1 / (int)L1D.size() - (1 - (double)I2 / (int)L2D.size()));
+
+                  if(D < BestRD)
+                  {
+                     BestI2R = I2;
+                     BestRD = D;
+                  }
+               }
+
+               double D = fabs(L1D[I1] - L2D[BestI2R]);
+               
+               if(BestD < 0 || D < BestD)
+               {
+                  BestI1 = I1;
+                  BestI2 = BestI2R;
+                  BestD = D;
+               }
+            }
+
+            double P1 = (double)BestI1 / L1D.size();
+            double P2 = 1 - (double)BestI2 / L2D.size();
+
+            ModelP = (P1 + P2) / 2;
+
+            if(ModelP > 0.5)
+               ModelP = 1 - ModelP;
+
+            if(Swapped == true)
+               swap(L1D, L2D);
          }
 
          out << iM1 << " " << iM2
@@ -262,9 +321,11 @@ Triplet GetTriplet(int x, int N)
 {
    Triplet Result;
 
+   pair<double, double> Range = BinomialRange(x, N);
+
    Result.Center = (double)x / N;
-   Result.Low = Result.Center / 2;
-   Result.High = Result.Center + (1 - Result.Center) / 2;
+   Result.Low = Range.first;
+   Result.High = Range.second;
 
    return Result;
 }
@@ -275,8 +336,106 @@ ostream &operator <<(ostream &out, Triplet &T)
    return out;
 }
 
+pair<double, double> BinomialRange(int x, int N)
+{
+   if(N > 10000)
+   {
+      pair<double, double> FakeRange;
+      double r = (double)x / N;
+      FakeRange.first = max(0.0, r - r * (1 / sqrt(max(x, 1)) + 1 / sqrt(N)));
+      FakeRange.second = min(1.0, r + r * (1 / sqrt(max(x, 1)) + 1 / sqrt(N)));
+      return FakeRange;
+   }
 
+   double Max = JntBinomial(x, N, 1);
 
+   double BestR = (double)x / N;
+   double LogBestHeight = x * log(BestR) + (N - x) * log(1 - BestR);
+   if(x == 0)
+      LogBestHeight = (N - x) * log(1 - BestR);
+   if(x == N)
+      LogBestHeight = x * log(BestR);
+
+   pair<double, double> Range(0, 1);
+
+   double LogHeightL = LogBestHeight - 20, LogHeightR = LogBestHeight;
+   while(LogHeightR - LogHeightL > 1e-6)
+   {
+      double LogHeightC = (LogHeightL + LogHeightR) / 2;
+
+      double L = 0, R = BestR;
+      while(R - L > 1e-8)
+      {
+         double C = (L + R) / 2;
+         double LH = x * log(C) + (N - x) * log(1 - C);
+         if(LH < LogHeightC)
+            L = C;
+         else
+            R = C;
+      }
+      Range.first = (L + R) / 2;
+
+      L = BestR, R = 1;
+      while(R - L > 1e-8)
+      {
+         double C = (L + R) / 2;
+         double LH = x * log(C) + (N - x) * log(1 - C);
+         if(LH >= LogHeightC)
+            L = C;
+         else
+            R = C;
+      }
+      Range.second = (L + R) / 2;
+
+      double Check = JntBinomial(x, N, Range.second) - JntBinomial(x, N, Range.first);
+      if(Check < 0.90 * Max)   // Range too small, lower height
+         LogHeightR = LogHeightC;
+      else
+         LogHeightL = LogHeightC;
+   }
+
+   return Range;
+}
+
+double JntBinomial(int x, int N, double r)
+{
+   // This calculates C(N,x) * \int r^x (1-r)^(N-x) dr using a recursion relationship
+
+   if(r == 0)
+      return 0;
+
+   if(x == 0)
+      return (1 - pow(1 - r, N + 1)) / (N + 1);
+
+   double LogExtraTerm = x * log(r) + (N - x + 1) * log(1 - r);
+   LogExtraTerm = LogExtraTerm + LogFactorial(N);
+   LogExtraTerm = LogExtraTerm - LogFactorial(x);
+   LogExtraTerm = LogExtraTerm - LogFactorial(N - x + 1);
+
+   if(r == 1)   // there is no extra term when r = 1
+      return JntBinomial(x - 1, N, r);
+
+   return JntBinomial(x - 1, N, r) - exp(LogExtraTerm);
+}
+
+double LogFactorial(int N)
+{
+   static double Result[100000] = {0};
+
+   if(N == 0)   // log(0) = 0
+      return 0;
+   if(N == 1)   // log(1) = 0
+      return 0;
+
+   if(Result[N] == 0)
+   {
+      double Answer = LogFactorial(N - 1) + log(N);
+      Result[N] = Answer;
+      return Answer;
+   }
+
+   return Result[N];
+}
 
 
 

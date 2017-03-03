e6ce9bd5fe97ae4c926e43e361cdcd66896bebf7
Yi <chen.yi.first@gmail.com>
2017-03-03 13:03:51 +0100

    Removed individual result files, commenting out the hadd part of the script to avoid killing the added trees

--- HIJetMass/JetStudies/15474_GroomingBasicProperties/Plots/RunAll.sh
+++ HIJetMass/JetStudies/15474_GroomingBasicProperties/Plots/RunAll.sh
@@ -1,9 +1,9 @@
-hadd -f -k PP6Dijet.root Result/PP6Dijet*.root 
-hadd -f -k PP8Dijet.root Result/PP8Dijet*.root 
-hadd -f -k AA6Dijet.root Result/AA6Dijet*.root 
-hadd -f -k AA8Dijet.root Result/AA8Dijet*.root 
-hadd -f -k PPDataHighPTJet.root Result/PPDataHighPTJet*.root 
-hadd -f -k AAData.root Result/AAData*.root 
+# hadd -f -k PP6Dijet.root Result/PP6Dijet*.root 
+# hadd -f -k PP8Dijet.root Result/PP8Dijet*.root 
+# hadd -f -k AA6Dijet.root Result/AA6Dijet*.root 
+# hadd -f -k AA8Dijet.root Result/AA8Dijet*.root 
+# hadd -f -k PPDataHighPTJet.root Result/PPDataHighPTJet*.root 
+# hadd -f -k AAData.root Result/AAData*.root 
 ./a.out PP6Dijet.root Plots/PP6Dijet
 ./a.out PP8Dijet.root Plots/PP8Dijet
 ./a.out AA6Dijet.root Plots/AA6Dijet

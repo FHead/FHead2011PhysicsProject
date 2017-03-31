ef7c62bcf0bcf09ab2b3f2cdab631da3ac405a71
Yi <chen.yi.first@gmail.com>
2017-03-31 16:13:16 +0200

    Simple hypothesis testing with 4e

---
+++ HiggsProperties/LightBosonStudies/16225_SimpleHypothesisTesting/compile.sh
@@ -0,0 +1,10 @@
+CompileRootMacro Likelihood.cpp Likelihood.o -std=c++11 -c \
+   -I../../CommonCode/include
+CompileRootMacro EvaluateLikelihood.cpp RunLikelihood -std=c++11 \
+   -I ../../CommonCode/include \
+   ../../CommonCode/library/Cuts.o \
+   ../../CommonCode/library/Lists.o \
+   ../../CommonCode/library/AngleConversion.o \
+   ../../CommonCode/library/EFTBases.o \
+   ../../CommonCode/library/InterpretCoefficient.o \
+   Likelihood.o

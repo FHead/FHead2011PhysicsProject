04da40d3ba5eaa9b57599f69ae75793c8ba209aa
Yi <chen.yi.first@gmail.com>
2017-05-04 12:49:14 +0200

    validation for normalization

--- HiggsProperties/BuildMap/16261_NormalizationValidation/compile.sh
+++ HiggsProperties/BuildMap/16261_NormalizationValidation/compile.sh
@@ -2,3 +2,7 @@ CompileRootMacro CheckNormalization.cpp Run -std=c++11 \
    -I ../../CommonCode/include \
    ../../CommonCode/library/Cuts.o \
    ../../CommonCode/library/AngleConversion.o
+CompileRootMacro CheckSomeAs.cpp RunAs -std=c++11 \
+   -I ../../CommonCode/include \
+   ../../CommonCode/library/Cuts.o \
+   ../../CommonCode/library/AngleConversion.o

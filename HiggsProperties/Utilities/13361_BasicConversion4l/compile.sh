22433c33835bc0772e4f826e20e8889b4571f07f
Yi <chen.yi.first@gmail.com>
2017-05-04 12:52:10 +0200

    compilation code for basic conversion

--- HiggsProperties/Utilities/13361_BasicConversion4l/compile.sh
+++ HiggsProperties/Utilities/13361_BasicConversion4l/compile.sh
@@ -1,13 +1,13 @@
 g++ ConvertVectorsToAnglesRoberto.cpp -o ConvertVectorsToAnglesRoberto \
-   -I$HOME/PhysicsWorkspace/HiggsProperties/CommonCode/include \
+   -I$PhysicsWorkspace/HiggsProperties/CommonCode/include \
    -I$RootMacrosBase \
-   $HOME/PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
+   $PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
    $RootMacrosBase/Code/library/TauHelperFunctions3.o
 
 g++ ConvertXYZToVectors.cpp -o ConvertXYZToVectors \
-   -I$HOME/PhysicsWorkspace/HiggsProperties/CommonCode/include \
+   -I$PhysicsWorkspace/HiggsProperties/CommonCode/include \
    -I$RootMacrosBase \
-   $HOME/PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
+   $PhysicsWorkspace/HiggsProperties/CommonCode/library/AngleConversion.o \
    $RootMacrosBase/Code/library/TauHelperFunctions3.o
 
 

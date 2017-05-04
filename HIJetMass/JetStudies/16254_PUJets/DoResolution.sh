c7cca320b840a59e7cd3506112ff8bc888430e32
Yi Chen <chen.yi.first@gmail.com>
2017-05-04 13:04:09 +0200

    studies with PU jets

---
+++ HIJetMass/JetStudies/16254_PUJets/DoResolution.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# CompileRootMacro CalculateResolution.cpp
+# 
+# ./a.out Y
+# ./a.out N
+
+
+CompileRootMacro FitResolutionMeanRMS.cpp
+
+./a.out AA6DijetCymbal
+./a.out AADataPrescale
+
+

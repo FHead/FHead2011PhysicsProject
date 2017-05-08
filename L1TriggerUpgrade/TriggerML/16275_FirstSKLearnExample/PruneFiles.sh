efb3986198521cabe4f0b74e97aadb8606044b18
Yi Chen <chen.yi.first@gmail.com>
2017-05-08 10:17:17 +0200

    removed prune file script - it is now done in the master run all script

--- L1TriggerUpgrade/TriggerML/16275_FirstSKLearnExample/PruneFiles.sh
+++
@@ -1,2 +0,0 @@
-awk '{if($7 != 20 && $7 != 19 && $7 != -20 && $7 != -19) print}' < WToEnu140PU.txt  > WToEnu140PUPruned.txt
-awk '{if($7 != 20 && $7 != 19 && $7 != -20 && $7 != -19) print}' < WToEnu200PU.txt  > WToEnu200PUPruned.txt

3b11a3dbc320904f7867a281251731d720c11796
Yi <chen.yi.first@gmail.com>
2017-05-10 14:07:24 +0200

    new smearing with fast jet contrib version 1.1.1

---
+++ HIJetMass/JetStudies/16313_PPSmearingFastJet111/compile.sh
@@ -0,0 +1,10 @@
+export FASTJET=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/fastjet/3.0.3-cms2/bin/
+# export FASTCONTRIB=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/fastjet-contrib/1.014/
+export FASTCONTRIB=/afs/cern.ch/user/c/chenyi/Programs/fastjet/install
+
+echo Compiling RunSmear
+CompileRootMacro JetMassPlots.cpp RunSmear \
+   -I../../CommonCode/include ../../CommonCode/library/* \
+   `$FASTJET/fastjet-config --cxxflags` `$FASTJET/fastjet-config --libs` \
+   -I$FASTCONTRIB/include $FASTCONTRIB/lib/*.a
+

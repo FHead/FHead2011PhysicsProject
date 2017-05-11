9cdd2005b1a01992b0d7f268237243b687b6a76d
Yi Chen <chen.yi.first@gmail.com>
2017-05-11 12:16:23 +0200

    Some debug messages

--- HIJetMass/JetStudies/16314_CaloPUJets/MakePlainTree.cpp
+++ HIJetMass/JetStudies/16314_CaloPUJets/MakePlainTree.cpp
@@ -344,6 +344,7 @@ int main(int argc, char *argv[])
             }
          }
 
+         cout << "Number of calo PU jet = " << MCaloPUJet.JetCount << endl;
          cout << BestIndex << endl;
 
          if(BestIndex >= 0)
@@ -352,6 +353,8 @@ int main(int argc, char *argv[])
             TreeJ60CaloPUEta = MCaloPUJet.JetEta[BestIndex];
             TreeJ60CaloPUPhi = MCaloPUJet.JetPhi[BestIndex];
          }
+
+         cout << TreeJ60CaloPUPT << " " << TreeJ60CaloPUEta << " " << TreeJ60CaloPUPhi << endl;
       }
 
       TreeJ80PT = -1, TreeJ80Eta = -1, TreeJ80Phi = -1;

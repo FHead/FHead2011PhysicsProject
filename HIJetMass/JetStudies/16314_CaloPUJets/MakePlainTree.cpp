0c5f5e86fd3c0686b653d966bbb30ca995090187
Yi Chen <chen.yi.first@gmail.com>
2017-05-11 11:50:54 +0200

    added trigger object and matching

--- HIJetMass/JetStudies/16314_CaloPUJets/MakePlainTree.cpp
+++ HIJetMass/JetStudies/16314_CaloPUJets/MakePlainTree.cpp
@@ -53,7 +53,9 @@ int main(int argc, char *argv[])
    string CaloPUJetTreeName = "akPu4CaloJetAnalyzer/t";
    string SoftDropJetTreeName = "akCsSoftDrop4PFJetAnalyzer/t";
    string PFTreeName = "pfcandAnalyzerCS/pfTree";
-   string 
+   string J60ObjectTreeName = "hltobject/HLT_HIPuAK4CaloJet60_Eta5p1_v";
+   string J80ObjectTreeName = "hltobject/HLT_HIPuAK4CaloJet80_Eta5p1_v";
+   string J100ObjectTreeName = "hltobject/HLT_HIPuAK4CaloJet100_Eta5p1_v";
 
    if(IsPP == true)
    {
@@ -73,6 +75,9 @@ int main(int argc, char *argv[])
    RhoTreeMessenger MRho(InputFile);
    TriggerTreeMessenger MHLT(InputFile);
    SkimTreeMessenger MSkim(InputFile);
+   TriggerObjectTreeMessenger MJ60(InputFile, J60ObjectTreeName);
+   TriggerObjectTreeMessenger MJ80(InputFile, J80ObjectTreeName);
+   TriggerObjectTreeMessenger MJ100(InputFile, J100ObjectTreeName);
 
    if(MHiEvent.Tree == NULL)
       return -1;
@@ -106,20 +111,20 @@ int main(int argc, char *argv[])
    OutputTree.Branch("JetPUPT", &TreeJetPUPT, "JetPUPT/D");
    OutputTree.Branch("JetPUEta", &TreeJetPUEta, "JetPUEta/D");
    OutputTree.Branch("JetPUPhi", &TreeJetPUPhi, "JetPUPhi/D");
-   
+
    double TreeJetCaloPUMatchDR, TreeJetCaloPUPT, TreeJetCaloPURawPT, TreeJetCaloPUEta, TreeJetCaloPUPhi;
    OutputTree.Branch("JetCaloPUMatchDR", &TreeJetCaloPUMatchDR, "JetCaloPUMatchDR/D");
    OutputTree.Branch("JetCaloPURawPT", &TreeJetCaloPURawPT, "JetCaloPURawPT/D");
    OutputTree.Branch("JetCaloPUPT", &TreeJetCaloPUPT, "JetCaloPUPT/D");
    OutputTree.Branch("JetCaloPUEta", &TreeJetCaloPUEta, "JetCaloPUEta/D");
    OutputTree.Branch("JetCaloPUPhi", &TreeJetCaloPUPhi, "JetCaloPUPhi/D");
-   
+
    double TreeJetPT, TreeJetRawPT, TreeJetEta, TreeJetPhi;
    OutputTree.Branch("JetPT", &TreeJetPT, "JetPT/D");
    OutputTree.Branch("JetRawPT", &TreeJetRawPT, "JetRawPT/D");
    OutputTree.Branch("JetEta", &TreeJetEta, "JetEta/D");
    OutputTree.Branch("JetPhi", &TreeJetPhi, "JetPhi/D");
-   
+
    double TreeSubJet1PT, TreeSubJet1Eta, TreeSubJet1Phi;
    double TreeSubJet2PT, TreeSubJet2Eta, TreeSubJet2Phi;
    OutputTree.Branch("SubJet1PT", &TreeSubJet1PT, "SubJet1PT/D");
@@ -152,7 +157,7 @@ int main(int argc, char *argv[])
    OutputTree.Branch("SDMass0", &TreeSDMass0, "SDMass0/D");
    OutputTree.Branch("SubJetDR7", &TreeSubJetDR7, "SubJetDR7/D");
    OutputTree.Branch("SDMass7", &TreeSDMass7, "SDMass7/D");
-   
+
    double TreeSubJet1PT0, TreeSubJet1Eta0, TreeSubJet1Phi0;
    double TreeSubJet2PT0, TreeSubJet2Eta0, TreeSubJet2Phi0;
    OutputTree.Branch("SubJet1PT0", &TreeSubJet1PT0, "SubJet1PT0/D");
@@ -161,7 +166,7 @@ int main(int argc, char *argv[])
    OutputTree.Branch("SubJet2PT0", &TreeSubJet2PT0, "SubJet2PT0/D");
    OutputTree.Branch("SubJet2Eta0", &TreeSubJet2Eta0, "SubJet2Eta0/D");
    OutputTree.Branch("SubJet2Phi0", &TreeSubJet2Phi0, "SubJet2Phi0/D");
-   
+
    double TreeSubJet1PT7, TreeSubJet1Eta7, TreeSubJet1Phi7;
    double TreeSubJet2PT7, TreeSubJet2Eta7, TreeSubJet2Phi7;
    OutputTree.Branch("SubJet1PT7", &TreeSubJet1PT7, "SubJet1PT7/D");
@@ -183,13 +188,56 @@ int main(int argc, char *argv[])
    OutputTree.Branch("CEF", &TreeCEF, "CEF/D");
    OutputTree.Branch("NEF", &TreeNEF, "NEF/D");
    OutputTree.Branch("MUF", &TreeMUF, "MUF/D");
-   
+
    double TreeCHM, TreeNHM, TreeCEM, TreeNEM, TreeMUM;
    OutputTree.Branch("CHM", &TreeCHM, "CHM/D");
    OutputTree.Branch("NHM", &TreeNHM, "NHM/D");
    OutputTree.Branch("CEM", &TreeCEM, "CEM/D");
    OutputTree.Branch("NEM", &TreeNEM, "NEM/D");
    OutputTree.Branch("MUM", &TreeMUM, "MUM/D");
+
+   int TreeJetIndex;
+   OutputTree.Branch("JetIndex", &TreeJetIndex, "TreeJetIndex/I");
+
+   // Trigger objects
+   double TreeJ60PT, TreeJ60Eta, TreeJ60Phi;
+   double TreeJ80PT, TreeJ80Eta, TreeJ80Phi;
+   double TreeJ100PT, TreeJ100Eta, TreeJ100Phi;
+   OutputTree.Branch("J60PT", &TreeJ60PT, "J60PT/D");
+   OutputTree.Branch("J60Eta", &TreeJ60Eta, "J60Eta/D");
+   OutputTree.Branch("J60Phi", &TreeJ60Phi, "J60Phi/D");
+   OutputTree.Branch("J80PT", &TreeJ80PT, "J80PT/D");
+   OutputTree.Branch("J80Eta", &TreeJ80Eta, "J80Eta/D");
+   OutputTree.Branch("J80Phi", &TreeJ80Phi, "J80Phi/D");
+   OutputTree.Branch("J100PT", &TreeJ100PT, "J100PT/D");
+   OutputTree.Branch("J100Eta", &TreeJ100Eta, "J100Eta/D");
+   OutputTree.Branch("J100Phi", &TreeJ100Phi, "J100Phi/D");
+
+   double TreeJ60CaloPUPT, TreeJ60CaloPUEta, TreeJ60CaloPUPhi;
+   double TreeJ80CaloPUPT, TreeJ80CaloPUEta, TreeJ80CaloPUPhi;
+   double TreeJ100CaloPUPT, TreeJ100CaloPUEta, TreeJ100CaloPUPhi;
+   OutputTree.Branch("J60CaloPUPT", &TreeJ60CaloPUPT, "J60CaloPUPT/D");
+   OutputTree.Branch("J60CaloPUEta", &TreeJ60CaloPUEta, "J60CaloPUEta/D");
+   OutputTree.Branch("J60CaloPUPhi", &TreeJ60CaloPUPhi, "J60CaloPUPhi/D");
+   OutputTree.Branch("J80CaloPUPT", &TreeJ80CaloPUPT, "J80CaloPUPT/D");
+   OutputTree.Branch("J80CaloPUEta", &TreeJ80CaloPUEta, "J80CaloPUEta/D");
+   OutputTree.Branch("J80CaloPUPhi", &TreeJ80CaloPUPhi, "J80CaloPUPhi/D");
+   OutputTree.Branch("J100CaloPUPT", &TreeJ100CaloPUPT, "J100CaloPUPT/D");
+   OutputTree.Branch("J100CaloPUEta", &TreeJ100CaloPUEta, "J100CaloPUEta/D");
+   OutputTree.Branch("J100CaloPUPhi", &TreeJ100CaloPUPhi, "J100CaloPUPhi/D");
+
+   double TreeJ60PFPUPT, TreeJ60PFPUEta, TreeJ60PFPUPhi;
+   double TreeJ80PFPUPT, TreeJ80PFPUEta, TreeJ80PFPUPhi;
+   double TreeJ100PFPUPT, TreeJ100PFPUEta, TreeJ100PFPUPhi;
+   OutputTree.Branch("J60PFPUPT", &TreeJ60PFPUPT, "J60PFPUPT/D");
+   OutputTree.Branch("J60PFPUEta", &TreeJ60PFPUEta, "J60PFPUEta/D");
+   OutputTree.Branch("J60PFPUPhi", &TreeJ60PFPUPhi, "J60PFPUPhi/D");
+   OutputTree.Branch("J80PFPUPT", &TreeJ80PFPUPT, "J80PFPUPT/D");
+   OutputTree.Branch("J80PFPUEta", &TreeJ80PFPUEta, "J80PFPUEta/D");
+   OutputTree.Branch("J80PFPUPhi", &TreeJ80PFPUPhi, "J80PFPUPhi/D");
+   OutputTree.Branch("J100PFPUPT", &TreeJ100PFPUPT, "J100PFPUPT/D");
+   OutputTree.Branch("J100PFPUEta", &TreeJ100PFPUEta, "J100PFPUEta/D");
+   OutputTree.Branch("J100PFPUPhi", &TreeJ100PFPUPhi, "J100PFPUPhi/D");
 
    int EntryCount = MHiEvent.Tree->GetEntries();
    ProgressBar Bar(cout, EntryCount);
@@ -203,6 +251,7 @@ int main(int argc, char *argv[])
          Bar.Print();
       }
 
+      // Messengers
       MHiEvent.GetEntry(iE);
       MJet.GetEntry(iE);
       MSDJet.GetEntry(iE);
@@ -211,15 +260,21 @@ int main(int argc, char *argv[])
       MRho.GetEntry(iE);
       MHLT.GetEntry(iE);
       MSkim.GetEntry(iE);
-         
+      MJ60.GetEntry(iE);
+      MJ80.GetEntry(iE);
+      MJ100.GetEntry(iE);
+
+      // For normalization purposes
       HN.Fill(0);
 
+      // HF Stuff
       TreeHF = MHiEvent.hiHF;
       TreeHFPlus = MHiEvent.hiHFplus;
       TreeHFMinus = MHiEvent.hiHFminus;
       TreeHFPlusEta4 = MHiEvent.hiHFplusEta4;
       TreeHFMinusEta4 = MHiEvent.hiHFminusEta4;
-         
+
+      // Basic selection
       TreePassPbPb40 = MHLT.CheckTrigger("HLT_HIPuAK4CaloJet40_Eta5p1_v1");
       TreePassPbPb60 = MHLT.CheckTrigger("HLT_HIPuAK4CaloJet60_Eta5p1_v1");
       TreePassPbPb80 = MHLT.CheckTrigger("HLT_HIPuAK4CaloJet80_Eta5p1_v1");
@@ -240,6 +295,7 @@ int main(int argc, char *argv[])
 
       SDJetHelper HSDJet(MSDJet);
 
+      // Centrality and stuff
       TreeCentrality = GetCentrality(MHiEvent.hiBin);
       TreePTHat = MSDJet.PTHat;
       TreeRho = GetRho(MRho.EtaMax, MRho.Rho, 0);
@@ -247,20 +303,168 @@ int main(int argc, char *argv[])
       if(MSDJet.PTHat <= PTHatMin || MSDJet.PTHat > PTHatMax)
          continue;
 
+      // Trigger objects and matchings
+      TreeJ60PT = -1, TreeJ60Eta = -1, TreeJ60Phi = -1;
+      TreeJ60CaloPUPT = -1, TreeJ60Eta = -1, TreeJ60Phi = -1;
+      TreeJ60PFPUPT = -1, TreeJ60PFPUEta = -1, TreeJ60PFPUPhi = -1;
+      if(MJ60.ID != NULL && MJ60.ID->size() > 0)
+      {
+         TreeJ60PT = (*MJ60.PT)[0];   // I think the first one is always the largest PT - need to double check
+         TreeJ60Eta = (*MJ60.Eta)[0];
+         TreeJ60Phi = (*MJ60.Phi)[0];
+
+         int BestIndex = -1;
+         double BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ60Eta, TreeJ60Phi, MPUJet.JetEta[i], MPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ60PFPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ60PFPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ60PFPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+
+         BestIndex = -1;
+         BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ60Eta, TreeJ60Phi, MCaloPUJet.JetEta[i], MCaloPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ60CaloPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ60CaloPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ60CaloPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+      }
+
+      TreeJ80PT = -1, TreeJ80Eta = -1, TreeJ80Phi = -1;
+      TreeJ80CaloPUPT = -1, TreeJ80Eta = -1, TreeJ80Phi = -1;
+      TreeJ80PFPUPT = -1, TreeJ80PFPUEta = -1, TreeJ80PFPUPhi = -1;
+      if(MJ80.ID != NULL && MJ80.ID->size() > 0)
+      {
+         TreeJ80PT = (*MJ80.PT)[0];   // I think the first one is always the largest PT - need to double check
+         TreeJ80Eta = (*MJ80.Eta)[0];
+         TreeJ80Phi = (*MJ80.Phi)[0];
+
+         int BestIndex = -1;
+         double BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ80Eta, TreeJ80Phi, MPUJet.JetEta[i], MPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ80PFPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ80PFPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ80PFPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+
+         BestIndex = -1;
+         BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ80Eta, TreeJ80Phi, MCaloPUJet.JetEta[i], MCaloPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ80CaloPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ80CaloPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ80CaloPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+      }
+
+      TreeJ100PT = -1, TreeJ100Eta = -1, TreeJ100Phi = -1;
+      TreeJ100CaloPUPT = -1, TreeJ100Eta = -1, TreeJ100Phi = -1;
+      TreeJ100PFPUPT = -1, TreeJ100PFPUEta = -1, TreeJ100PFPUPhi = -1;
+      if(MJ100.ID != NULL && MJ100.ID->size() > 0)
+      {
+         TreeJ100PT = (*MJ100.PT)[0];   // I think the first one is always the largest PT - need to double check
+         TreeJ100Eta = (*MJ100.Eta)[0];
+         TreeJ100Phi = (*MJ100.Phi)[0];
+
+         int BestIndex = -1;
+         double BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ100Eta, TreeJ100Phi, MPUJet.JetEta[i], MPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ100PFPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ100PFPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ100PFPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+
+         BestIndex = -1;
+         BestDR2 = -1;
+         for(int i = 0; i < MPUJet.JetCount; i++)
+         {
+            double DR2 = GetDR2(TreeJ100Eta, TreeJ100Phi, MCaloPUJet.JetEta[i], MCaloPUJet.JetPhi[i]);
+            if(BestDR2 < 0 || BestDR2 > DR2)
+            {
+               BestIndex = i;
+               BestDR2 = DR2;
+            }
+         }
+
+         if(BestIndex >= 0)
+         {
+            TreeJ100CaloPUPT = MPUJet.JetPT[BestIndex];
+            TreeJ100CaloPUEta = MPUJet.JetEta[BestIndex];
+            TreeJ100CaloPUPhi = MPUJet.JetPhi[BestIndex];
+         }
+      }
+
+      // Cluster all the PF candidates
       vector<PseudoJet> Particles;
       for(int iPF = 0; iPF < MPF.ID->size(); iPF++)
       {
          FourVector P;
          P.SetPtEtaPhi((*MPF.PT)[iPF], (*MPF.Eta)[iPF], (*MPF.Phi)[iPF]);
-         // P[0] = (*MPF.E)[iPF];
          Particles.push_back(PseudoJet(P[1], P[2], P[3], P[0]));
       }
       JetDefinition Definition(antikt_algorithm, 0.4);
       ClusterSequence Sequence(Particles, Definition);
       vector<PseudoJet> Jets = Sequence.inclusive_jets();
 
+      // Loop over all SD jets
+      TreeJetIndex = 0;
       for(int iJ = 0; iJ < MSDJet.JetCount; iJ++)
       {
+         // Basic jet quantities
          TreeJetPT = MSDJet.JetPT[iJ];
          TreeJetRawPT = MSDJet.JetRawPT[iJ];
          TreeJetEta = MSDJet.JetEta[iJ];
@@ -279,6 +483,7 @@ int main(int argc, char *argv[])
          TreePartonFlavor = MSDJet.RefPartonFlavor[iJ];
          TreePartonFlavorForB = MSDJet.RefPartonFlavorForB[iJ];
 
+         // Match to CS jets to get PF jet ID
          int BestCSJet = 0;
          double BestCSDR = -1;
          for(int i = 0; i < MJet.JetCount; i++)
@@ -310,6 +515,7 @@ int main(int argc, char *argv[])
             TreeJetCSPhi = MJet.JetPhi[BestCSJet];
          }
 
+         // Match to PF PU jet
          int BestPUJet = 0;
          double BestPUDR = -1;
          for(int i = 0; i < MPUJet.JetCount; i++)
@@ -331,6 +537,7 @@ int main(int argc, char *argv[])
             TreeJetPUPhi = MPUJet.JetPhi[BestPUJet];
          }
 
+         // Match to Calo PU Jet
          int BestCaloPUJet = 0;
          double BestCaloPUDR = -1;
          for(int i = 0; i < MCaloPUJet.JetCount; i++)
@@ -352,6 +559,7 @@ int main(int argc, char *argv[])
             TreeJetCaloPUPhi = MCaloPUJet.JetPhi[BestCaloPUJet];
          }
 
+         // What is the closest reclustered jets - for additional SD settings
          int BestJet = 0;
          double BestDR = -1;
          for(int i = 0; i < (int)Jets.size(); i++)
@@ -398,7 +606,7 @@ int main(int argc, char *argv[])
             {
                TreeSubJetDR0 = GetDR(Groomed0->Child1->P, Groomed0->Child2->P);
                TreeSDMass0 = Groomed0->P.GetMass();
-         
+
                TreeSubJet1PT0 = Groomed0->Child1->P.GetPT();
                TreeSubJet1Eta0 = Groomed0->Child1->P.GetEta();
                TreeSubJet1Phi0 = Groomed0->Child1->P.GetPhi();
@@ -413,7 +621,7 @@ int main(int argc, char *argv[])
             {
                TreeSubJetDR7 = GetDR(Groomed7->Child1->P, Groomed7->Child2->P);
                TreeSDMass7 = Groomed7->P.GetMass();
-               
+
                TreeSubJet1PT7 = Groomed7->Child1->P.GetPT();
                TreeSubJet1Eta7 = Groomed7->Child1->P.GetEta();
                TreeSubJet1Phi7 = Groomed7->Child1->P.GetPhi();
@@ -430,6 +638,7 @@ int main(int argc, char *argv[])
             TreeMatchDR = -1;
 
          OutputTree.Fill();
+         TreeJetIndex = TreeJetIndex + 1;
       }
    }
 

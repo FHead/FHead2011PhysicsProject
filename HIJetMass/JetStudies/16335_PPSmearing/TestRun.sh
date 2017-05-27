8ee0bcf589b64fea7f9df99cdd51084b0e1edb2c
Yi <chen.yi.first@gmail.com>
2017-05-27 16:24:51 +0200

    PP smearing with direct MB event overlay

---
+++ HIJetMass/JetStudies/16335_PPSmearing/TestRun.sh
@@ -0,0 +1,1 @@
+./RunSmear PP6Dijet220Forest.root Meow.root PP6Dijet220 0 100000 MBCymbalForest.root 0.07 1.20 1 -1 -1
---
+++ HIJetMass/JetStudies/16335_PPSmearing/compile.sh
@@ -0,0 +1,12 @@
+export FASTJET=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/fastjet/3.0.3-cms2/bin/
+# export FASTCONTRIB=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/fastjet-contrib/1.014/
+# export FASTCONTRIB=/afs/cern.ch/user/c/chenyi/Programs/fastjet/install
+# export FASTJET=/afs/cern.ch/user/c/chenyi/work/Programs/fastjet/install/bin
+export FASTCONTRIB=/afs/cern.ch/user/c/chenyi/work/Programs/fastjet/install
+
+echo Compiling RunSmear
+CompileRootMacro JetMassPlots.cpp RunSmear \
+   -I../../CommonCode/include ../../CommonCode/library/* \
+   `$FASTJET/fastjet-config --cxxflags` `$FASTJET/fastjet-config --libs` \
+   -I$FASTCONTRIB/include $FASTCONTRIB/lib/*.a
+

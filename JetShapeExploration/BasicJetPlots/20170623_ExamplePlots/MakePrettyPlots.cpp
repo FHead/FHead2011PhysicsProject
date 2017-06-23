d8c8f77443770779a27e6901435f60845f819ba7
Yi Chen <chen.yi.first@gmail.com>
2017-06-23 16:36:55 +0200

    Make pretty plots

---
+++ JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/MakePrettyPlots.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+#include "TFile.h"
+#include "TH1D.h"
+
+#include "PlotHelper3.h"
+#include "SetStyle.h"
+
+int main()
+{
+   // Set plot style
+   SetThesisStyle();
+
+   // Output pdf file
+   PdfFileHelper PdfFile("PrettyPlots.pdf");
+   PdfFile.AddTextPage("Some first plots!");
+
+   // Input file
+   TFile File("ScaledResult/PP6Dijet.root");
+
+   // Add a bunch of histograms from the file
+   PdfFile.AddTextPage("PTHat Plots");
+   PdfFile.AddHistogramFromFile(File, "HPTHat", "", true);
+   PdfFile.AddHistogramFromFile(File, "HPTHatSelected", "", true);
+   
+   PdfFile.AddTextPage("Jet Plots");
+   PdfFile.AddHistogramFromFile(File, "HJetPT", "", true);
+   PdfFile.AddHistogramFromFile(File, "HJetEta", "", false);
+   PdfFile.AddHistogramFromFile(File, "HJetPhi", "", false);
+
+   // Cleanup
+   File.Close();
+
+   // Add a time stamp and close the pdf file
+   PdfFile.AddTimeStampPage();
+   PdfFile.Close();
+
+   return 0;
+}
+
+
+
--- JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/compile.sh
+++ JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/compile.sh
@@ -2,3 +2,8 @@ g++ RunExample.cpp -o RunExample \
    `root-config --libs` `root-config --cflags` \
    -I$RootMacrosBase $RootMacrosBase/Code/library/* \
    -I../../CommonCode/include ../../CommonCode/library/*
+
+g++ MakePrettyPlots.cpp -o MakePrettyPlot \
+   `root-config --libs` `root-config --cflags` \
+   -I$RootMacrosBase $RootMacrosBase/Code/library/* \
+   -I../../CommonCode/include ../../CommonCode/library/*

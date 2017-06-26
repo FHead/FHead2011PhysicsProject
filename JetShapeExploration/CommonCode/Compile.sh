4718353887a10c29c1ababe50d919ac853ab66a0
Yi Chen <chen.yi.first@gmail.com>
2017-06-26 12:25:57 +0200

    root macros base

--- JetShapeExploration/CommonCode/Compile.sh
+++ JetShapeExploration/CommonCode/Compile.sh
@@ -1,11 +1,11 @@
 mkdir -p bin library
 
 g++ source/BasicUtilities.cpp -I. -c -o library/BasicUtilities.o \
-   -I$HOME/FHeadHelperCode/RootMacros/
+   -I$RootMacrosBase/
 g++ source/Messenger.cpp -I. -c -o library/Messenger.o `root-config --cflags` -std=c++11
-g++ source/CATree.cpp -I. -c -o library/CATree.o -I$HOME/FHeadHelperCode/RootMacros/ -std=c++11
+g++ source/CATree.cpp -I. -c -o library/CATree.o -I$RootMacrosBase/ -std=c++11
 g++ source/SDJetHelper.cpp -I. -c -o library/SDJetHelper.o \
-   `root-config --cflags` -std=c++11 -I$HOME/FHeadHelperCode/RootMacros/
+   `root-config --cflags` -std=c++11 -I$RootMacrosBase/
 g++ source/EventMatching.cpp -I. -c -o library/EventMatching.o `root-config --cflags` -std=c++11
 
 g++ source/ScaleHistograms.cpp -o bin/ScaleHistograms `root-config --libs` `root-config --cflags`

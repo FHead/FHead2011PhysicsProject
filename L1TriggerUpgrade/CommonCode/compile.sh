42ecc2d5709cc479f2dc7511c615df0ad144cb71
Yi Chen <chen.yi.first@gmail.com>
2017-07-14 16:33:30 +0200

    Yay

--- L1TriggerUpgrade/CommonCode/compile.sh
+++ L1TriggerUpgrade/CommonCode/compile.sh
@@ -1,6 +1,6 @@
 mkdir -p library
 
 rootcint -f source/L1Classes.cpp -c include/L1Classes.h include/L1LinkDef.h
-CompileRootMacro source/L1Classes.cpp library/L1Classes.o -I. -c | grep -v warning
+g++ `root-config --cflags` source/L1Classes.cpp -o library/L1Classes.o -I. -c
 
 CompileRootMacro source/Messenger.cpp library/Messenger.o -Iinclude -c | grep -v warning

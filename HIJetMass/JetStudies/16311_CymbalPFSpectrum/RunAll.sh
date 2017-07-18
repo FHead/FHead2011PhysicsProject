0a46c192fae21d4eb92cf78198ae78bbf429a896
Yi <chen.yi.first@gmail.com>
2017-07-18 13:16:07 +0200

    16442 sync

--- HIJetMass/JetStudies/16311_CymbalPFSpectrum/RunAll.sh
+++
@@ -1,5 +0,0 @@
-# time ./RunSmear PP6Dijet_120_18.root Smear120Meow.root PP6Dijet 0 10000 1.00 0.07 0.20 1.00 1.20 0 1 -1 -1
-# time ./RunPlain AA6DijetCymbal_120_31.root Straight120.root AA6Dijet 0 10000
-time ./RunSmear PP6Dijet_200_4.root Smear200.root PP6Dijet 0 10000 1.00 0.07 0.20 1.00 1.20 0 1 -1 -1
-time ./RunPlain AA6DijetCymbal_200_25.root Straight200.root AA6Dijet 0 10000
-

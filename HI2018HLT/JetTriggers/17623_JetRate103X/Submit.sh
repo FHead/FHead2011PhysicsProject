b4a8460d6ce5bd9b91c0fcd20517289c25e355af
Yi <chen.yi.first@gmail.com>
2018-10-04 14:04:24 +0200

    some rate code

---
+++ HI2018HLT/JetTriggers/17623_JetRate103X/Submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Files=`cat FileList | tr '\n' ' '`
+
+ID=0
+for i in `echo $Files | Reformat 4 | tr ' ' '@'`
+do
+   ID=`echo $ID | AddConst 1`
+   echo $ID $i
+
+   cat Template.sh \
+      | sed "s#__INPUT__#$i#" \
+      | sed "s#__ID__#$ID#" | bsub -J RunHLTRate -M 4000000
+done
+
+
+
+

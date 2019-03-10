b6959d2d23fdda5965d492c0a41b2dd6059e6d50
FHead <chen.yi.first@gmail.com>
2019-03-10 14:07:18 +0100

    Common code for V7.3

---
+++ L1TriggerUpgrade2018/CommonCode/include/L1Classes.h
@@ -0,0 +1,47 @@
+#include "include/L1AnalysisCSCTFDataFormat.h"
+#include "include/L1AnalysisCaloTPDataFormat.h"
+#include "include/L1AnalysisDTTFDataFormat.h"
+#include "include/L1AnalysisEventDataFormat.h"
+#include "include/L1AnalysisGCTDataFormat.h"
+#include "include/L1AnalysisGMTDataFormat.h"
+#include "include/L1AnalysisGTDataFormat.h"
+#include "include/L1AnalysisGeneratorDataFormat.h"
+#include "include/L1AnalysisL1ExtraDataFormat.h"
+#include "include/L1AnalysisL1MenuDataFormat.h"
+#include "include/L1AnalysisRCTDataFormat.h"
+#include "include/L1AnalysisRecoClusterDataFormat.h"
+#include "include/L1AnalysisRecoJetDataFormat.h"
+#include "include/L1AnalysisRecoMetDataFormat.h"
+#include "include/L1AnalysisRecoMuonDataFormat.h"
+#include "include/L1AnalysisRecoTrackDataFormat.h"
+#include "include/L1AnalysisRecoVertexDataFormat.h"
+#include "include/L1AnalysisSimulationDataFormat.h"
+#include "include/L1AnalysisL1ExtraUpgradeDataFormat.h"
+#include "include/L1AnalysisPhaseIIDataFormatV7p3.h"
+
+namespace
+{
+   namespace
+   {
+      L1Analysis::L1AnalysisCSCTFDataFormat          DICT1;
+      L1Analysis::L1AnalysisCaloTPDataFormat         DICT2;
+      L1Analysis::L1AnalysisDTTFDataFormat           DICT3;
+      L1Analysis::L1AnalysisEventDataFormat          DICT5;
+      L1Analysis::L1AnalysisGCTDataFormat            DICT6;
+      L1Analysis::L1AnalysisGMTDataFormat            DICT7;
+      L1Analysis::L1AnalysisGTDataFormat             DICT8;
+      L1Analysis::L1AnalysisGeneratorDataFormat      DICT10;
+      L1Analysis::L1AnalysisL1ExtraDataFormat        DICT11;
+      L1Analysis::L1AnalysisL1MenuDataFormat         DICT12;
+      L1Analysis::L1AnalysisRCTDataFormat            DICT13;
+      L1Analysis::L1AnalysisRecoClusterDataFormat    DICT14;
+      L1Analysis::L1AnalysisRecoJetDataFormat        DICT15;
+      L1Analysis::L1AnalysisRecoMetDataFormat        DICT16;
+      L1Analysis::L1AnalysisRecoMuonDataFormat       DICT17;
+      L1Analysis::L1AnalysisRecoTrackDataFormat      DICT18;
+      L1Analysis::L1AnalysisRecoVertexDataFormat     DICT19;
+      L1Analysis::L1AnalysisSimulationDataFormat     DICT20;
+      L1Analysis::L1AnalysisL1ExtraUpgradeDataFormat DICT21;
+      L1Analysis::L1AnalysisPhaseIIDataFormat        DICT22;
+   };
+};

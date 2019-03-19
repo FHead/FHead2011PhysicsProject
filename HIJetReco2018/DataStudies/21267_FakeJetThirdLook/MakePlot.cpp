20988e0c7511241e1af28fd2d44d173a50e1bade
FHead <chen.yi.first@gmail.com>
2019-03-19 12:58:37 +0100

    track jet and track MET studies for fakes

---
+++ HIJetReco2018/DataStudies/21267_FakeJetThirdLook/MakePlot.cpp
@@ -0,0 +1,254 @@
+#include <iostream>
+using namespace std;
+
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TLegend.h"
+
+#include "SetStyle.h"
+#include "CommandLine.h"
+#include "PlotHelper4.h"
+
+int main(int argc, char *argv[]);
+void Add1DPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix);
+void AddComparisonPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix1, string Prefix2, string Prefix3, string Prefix4);
+void AddMirrorPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix);
+void Add2DPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix);
+
+int main(int argc, char *argv[])
+{
+   SetThesisStyle();
+
+   CommandLine CL(argc, argv);
+
+   string InputFileName = CL.Get("Input");
+   string OutputFileName = CL.Get("Output");
+
+   PdfFileHelper PdfFile(OutputFileName);
+   PdfFile.AddTextPage("Track plots");
+
+   TFile InputFile(InputFileName.c_str());
+
+   Add1DPlot(PdfFile, InputFile, "HJetCount");
+   Add1DPlot(PdfFile, InputFile, "HJetCountPT30");
+   Add1DPlot(PdfFile, InputFile, "HJetCountPT50");
+   Add1DPlot(PdfFile, InputFile, "HJetCountPT75");
+   Add1DPlot(PdfFile, InputFile, "HJetCountPT100");
+   Add1DPlot(PdfFile, InputFile, "HJetCountPT125");
+   Add1DPlot(PdfFile, InputFile, "HJetPT");
+   Add1DPlot(PdfFile, InputFile, "HJetPTEtaN25N15");
+   Add1DPlot(PdfFile, InputFile, "HJetPTEtaN15N00");
+   Add1DPlot(PdfFile, InputFile, "HJetPTEtaP00P15");
+   Add1DPlot(PdfFile, InputFile, "HJetPTEtaP15P25");
+   AddComparisonPlot(PdfFile, InputFile, "HJetPTEtaN25N15", "HJetPTEtaN15N00", "HJetPTEtaP00P15", "HJetPTEtaP15P25");
+   Add2DPlot(PdfFile, InputFile, "HJetEtaPhiPT30");
+   Add2DPlot(PdfFile, InputFile, "HJetEtaPhiPT50");
+   Add2DPlot(PdfFile, InputFile, "HJetEtaPhiPT75");
+   Add2DPlot(PdfFile, InputFile, "HJetEtaPhiPT100");
+   Add2DPlot(PdfFile, InputFile, "HJetEtaPhiPT125");
+
+   Add1DPlot(PdfFile, InputFile, "HTrackMETX");
+   Add1DPlot(PdfFile, InputFile, "HTrackMETY");
+   Add1DPlot(PdfFile, InputFile, "HTrackMET");
+   Add1DPlot(PdfFile, InputFile, "HTrackMETXPeripheral");
+   Add1DPlot(PdfFile, InputFile, "HTrackMETYPeripheral");
+   Add1DPlot(PdfFile, InputFile, "HTrackMETPeripheral");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMETX");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMETY");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMET");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMETXPeripheral");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMETYPeripheral");
+   Add1DPlot(PdfFile, InputFile, "HPositiveTrackMETPeripheral");
+   
+   AddMirrorPlot(PdfFile, InputFile, "HTrackMETX");
+   AddMirrorPlot(PdfFile, InputFile, "HTrackMETXPeripheral");
+   AddMirrorPlot(PdfFile, InputFile, "HPositiveTrackMETX");
+   AddMirrorPlot(PdfFile, InputFile, "HPositiveTrackMETXPeripheral");
+
+   InputFile.Close();
+
+   PdfFile.AddTimeStampPage();
+   PdfFile.Close();
+
+   return 0;
+}
+
+void Add1DPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix)
+{
+   PdfFile.AddTextPage(Prefix);
+
+   TCanvas Canvas;
+
+   vector<string> Labels = {"MVA090", "MVA091", "MVA092", "MVA093", "MVA094", "MVA095", "MVA096", "MVA097", "MVA098", "MVA099", "MVA100"};
+
+   TLegend Legend(0.6, 0.85, 0.85, 0.45);
+   Legend.SetTextFont(42);
+   Legend.SetTextSize(0.035);
+   Legend.SetBorderSize(0);
+   Legend.SetFillStyle(0);
+
+   vector<int> Colors = {kBlack, kRed, kBlue, kGreen + 3, kYellow + 3, kCyan, kBlack, kRed, kBlue, kGreen + 3, kYellow + 3};
+   vector<int> Styles = {kSolid, kSolid, kSolid, kSolid, kSolid, kSolid, kDashed, kDashed, kDashed, kDashed, kDashed};
+
+   string Option = "";
+   for(int i = 0; i < 11; i++)
+   {
+      TH1D *H = (TH1D *)File.Get(Form("%s%s", Prefix.c_str(), Labels[i].c_str()));
+      
+      if(H == nullptr)
+         continue;
+
+      H->SetLineWidth(2);
+      H->SetLineColor(Colors[i]);
+      H->SetLineStyle(Styles[i]);
+
+      H->SetStats(0);
+      H->Draw(Option.c_str());
+      Option = "same";
+
+      Legend.AddEntry(H, Labels[i].c_str(), "l");
+   }
+
+   Legend.Draw();
+
+   PdfFile.AddCanvas(Canvas);
+   Canvas.SetLogy();
+   PdfFile.AddCanvas(Canvas);
+}
+
+void AddComparisonPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix1, string Prefix2, string Prefix3, string Prefix4)
+{
+   vector<string> Prefix(4);
+   Prefix[0] = Prefix1;
+   Prefix[1] = Prefix2;
+   Prefix[2] = Prefix3;
+   Prefix[3] = Prefix4;
+   PdfFile.AddTextPage(Prefix);
+
+   TCanvas Canvas;
+
+   vector<string> Labels = {"MVA090", "MVA091", "MVA092", "MVA093", "MVA094", "MVA095", "MVA096", "MVA097", "MVA098", "MVA099", "MVA100"};
+
+   for(int i = 0; i < 11; i++)
+   {
+      TH1D *H1 = (TH1D *)File.Get(Form("%s%s", Prefix1.c_str(), Labels[i].c_str()));
+      TH1D *H2 = (TH1D *)File.Get(Form("%s%s", Prefix2.c_str(), Labels[i].c_str()));
+      TH1D *H3 = (TH1D *)File.Get(Form("%s%s", Prefix3.c_str(), Labels[i].c_str()));
+      TH1D *H4 = (TH1D *)File.Get(Form("%s%s", Prefix4.c_str(), Labels[i].c_str()));
+      
+      if(H1 == nullptr || H2 == nullptr || H3 == nullptr || H4 == nullptr)
+         continue;
+
+      H1->SetStats(0);
+      H1->SetTitle(Form("%s", Labels[i].c_str()));
+
+      H1->SetLineWidth(2);
+      H2->SetLineWidth(2);
+      H3->SetLineWidth(2);
+      H4->SetLineWidth(2);
+      
+      H1->SetLineColor(kMagenta);
+      H2->SetLineColor(kBlack);
+      H3->SetLineColor(kGreen + 3);
+      H4->SetLineColor(kCyan);
+      
+      H1->SetLineStyle(kSolid);
+      H2->SetLineStyle(kSolid);
+      H3->SetLineStyle(kSolid);
+      H4->SetLineStyle(kSolid);
+
+      H1->Draw();
+      H2->Draw("same");
+      H3->Draw("same");
+      H4->Draw("same");
+
+      TLegend Legend(0.5, 0.8, 0.8, 0.6);
+      Legend.SetTextFont(42);
+      Legend.SetTextSize(0.035);
+      Legend.SetBorderSize(0);
+      Legend.SetFillStyle(0);
+      Legend.AddEntry(H1, "-2.5 < #eta < -1.5", "l");
+      Legend.AddEntry(H2, "-1.5 < #eta < 0.0", "l");
+      Legend.AddEntry(H3, "0.0 < #eta < 1.5", "l");
+      Legend.AddEntry(H4, "1.5 < #eta < 2.5", "l");
+      Legend.Draw();
+
+      Canvas.SetLogy();
+      PdfFile.AddCanvas(Canvas);
+   }
+}
+
+void AddMirrorPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix)
+{
+   PdfFile.AddTextPage(Prefix);
+
+   TCanvas Canvas;
+
+   vector<string> Labels = {"MVA090", "MVA091", "MVA092", "MVA093", "MVA094", "MVA095", "MVA096", "MVA097", "MVA098", "MVA099", "MVA100"};
+
+   for(int i = 0; i < 11; i++)
+   {
+      TH1D *H1 = (TH1D *)File.Get(Form("%s%s", Prefix.c_str(), Labels[i].c_str()));
+      
+      if(H1 == nullptr)
+         continue;
+
+      int N = H1->GetNbinsX();
+
+      TH1D *H2 = (TH1D *)H1->Clone("Mirror");
+      for(int iB = 1; iB <= N; iB++)
+         H2->SetBinContent(iB, H1->GetBinContent(N - (iB - 1)));
+
+      H1->SetStats(0);
+      H1->SetTitle(Form("%s", Labels[i].c_str()));
+
+      H1->SetLineWidth(2);
+      H2->SetLineWidth(2);
+      
+      H1->SetLineColor(kMagenta);
+      H2->SetLineColor(kCyan);
+      
+      H1->SetLineStyle(kSolid);
+      H2->SetLineStyle(kSolid);
+
+      H1->Draw();
+      H2->Draw("same");
+
+      TLegend Legend(0.5, 0.8, 0.8, 0.6);
+      Legend.SetTextFont(42);
+      Legend.SetTextSize(0.035);
+      Legend.SetBorderSize(0);
+      Legend.SetFillStyle(0);
+      Legend.AddEntry(H1, "Plot", "l");
+      Legend.AddEntry(H2, "Mirror", "l");
+      Legend.Draw();
+
+      Canvas.SetLogy();
+      PdfFile.AddCanvas(Canvas);
+      
+      delete H2;
+   }
+}
+
+void Add2DPlot(PdfFileHelper &PdfFile, TFile &File, string Prefix)
+{
+   PdfFile.AddTextPage(Prefix);
+
+   vector<string> Labels = {"MVA090", "MVA091", "MVA092", "MVA093", "MVA094", "MVA095", "MVA096", "MVA097", "MVA098", "MVA099", "MVA100"};
+
+   for(int i = 0; i < 11; i++)
+   {
+      TH1D *H = (TH1D *)File.Get(Form("%s%s", Prefix.c_str(), Labels[i].c_str()));
+      
+      if(H == nullptr)
+         continue;
+
+      H->SetStats(0);
+      H->SetTitle(Form("%s, (%s)", H->GetTitle(), Labels[i].c_str()));
+      PdfFile.AddPlot(H, "colz");
+   }
+}
+
+
+
---
+++ HIJetReco2018/DataStudies/21267_FakeJetThirdLook/QuickLook.cpp
@@ -0,0 +1,301 @@
+#include <iostream>
+using namespace std;
+
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/ClusterSequenceArea.hh"
+using namespace fastjet;
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TGraphAsymmErrors.h"
+#include "TLegend.h"
+#include "TPaveStats.h"
+#include "TF1.h"
+#include "TLatex.h"
+
+#include "Code/TauHelperFunctions3.h"
+#include "ProgressBar.h"
+#include "PlotHelper4.h"
+#include "SetStyle.h"
+#include "CommandLine.h"
+
+#include "Messenger.h"
+
+int main(int argc, char *argv[])
+{
+   SetThesisStyle();
+
+   ClusterSequence::set_fastjet_banner_stream(NULL);
+
+   CommandLine CL(argc, argv);
+
+   vector<string> InputFileName  = CL.GetStringVector("Input", "");
+   string OutputFileName         = CL.Get("Output", "Result.root");
+
+   TFile OutputFile(OutputFileName.c_str(), "RECREATE");
+
+   vector<double> MVACut = {0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1.00};
+   vector<string> MVALabel = {"MVA090", "MVA091", "MVA092", "MVA093", "MVA094", "MVA095", "MVA096", "MVA097", "MVA098", "MVA099", "MVA100"};
+   // vector<double> MVACut = {0.90, 0.95, 0.98, 1.00};
+   // vector<string> MVALabel = {"MVA090", "MVA095", "MVA098", "MVA100"};
+
+   TH1D HN("HN", "", 1, 0, 1);
+   
+   vector<TH1D *> HJetCount(MVALabel.size());
+   vector<TH1D *> HJetCountPT30(MVALabel.size());
+   vector<TH1D *> HJetCountPT50(MVALabel.size());
+   vector<TH1D *> HJetCountPT75(MVALabel.size());
+   vector<TH1D *> HJetCountPT100(MVALabel.size());
+   vector<TH1D *> HJetCountPT125(MVALabel.size());
+   vector<TH1D *> HJetPT(MVALabel.size());
+   vector<TH1D *> HLeadingJetPT(MVALabel.size());
+   vector<TH1D *> HJetPTEtaN25N15(MVALabel.size());
+   vector<TH1D *> HJetPTEtaN15N00(MVALabel.size());
+   vector<TH1D *> HJetPTEtaP00P15(MVALabel.size());
+   vector<TH1D *> HJetPTEtaP15P25(MVALabel.size());
+   vector<TH2D *> HJetEtaPhiPT30(MVALabel.size());
+   vector<TH2D *> HJetEtaPhiPT50(MVALabel.size());
+   vector<TH2D *> HJetEtaPhiPT75(MVALabel.size());
+   vector<TH2D *> HJetEtaPhiPT100(MVALabel.size());
+   vector<TH2D *> HJetEtaPhiPT125(MVALabel.size());
+   vector<TH1D *> HTrackMETX(MVALabel.size());
+   vector<TH1D *> HTrackMETY(MVALabel.size());
+   vector<TH1D *> HTrackMET(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMETX(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMETY(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMET(MVALabel.size());
+   vector<TH1D *> HTrackMETXPeripheral(MVALabel.size());
+   vector<TH1D *> HTrackMETYPeripheral(MVALabel.size());
+   vector<TH1D *> HTrackMETPeripheral(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMETXPeripheral(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMETYPeripheral(MVALabel.size());
+   vector<TH1D *> HPositiveTrackMETPeripheral(MVALabel.size());
+
+   for(int iM = 0; iM < (int)MVALabel.size(); iM++)
+   {
+      HJetCount[iM] = new TH1D(Form("HJetCount%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetCountPT30[iM] = new TH1D(Form("HJetCountPT30%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetCountPT50[iM] = new TH1D(Form("HJetCountPT50%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetCountPT75[iM] = new TH1D(Form("HJetCountPT75%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetCountPT100[iM] = new TH1D(Form("HJetCountPT100%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetCountPT125[iM] = new TH1D(Form("HJetCountPT125%s", MVALabel[iM].c_str()), ";Jet count;", 100, 0, 100);
+      HJetPT[iM] = new TH1D(Form("HJetPT%s", MVALabel[iM].c_str()), ";Jet p_{T};", 100, 0, 400);
+      HLeadingJetPT[iM] = new TH1D(Form("HLeadingJetPT%s", MVALabel[iM].c_str()), ";Leading jet p_{T};", 100, 0, 400);
+      HJetPTEtaN25N15[iM] = new TH1D(Form("HJetPTEtaN25N15%s", MVALabel[iM].c_str()), "-2.5 < #eta < -1.5;Jet p_{T};", 100, 0, 400);
+      HJetPTEtaN15N00[iM] = new TH1D(Form("HJetPTEtaN15N00%s", MVALabel[iM].c_str()), "-1.5 < #eta < 0.0;Jet p_{T};", 100, 0, 400);
+      HJetPTEtaP00P15[iM] = new TH1D(Form("HJetPTEtaP00P15%s", MVALabel[iM].c_str()), "0.0 < #eta < 1.5;Jet p_{T};", 100, 0, 400);
+      HJetPTEtaP15P25[iM] = new TH1D(Form("HJetPTEtaP15P25%s", MVALabel[iM].c_str()), "1.5 < #eta < 2.5;Jet p_{T};", 100, 0, 400);
+      HJetEtaPhiPT30[iM] = new TH2D(Form("HJetEtaPhiPT30%s", MVALabel[iM].c_str()), "Jet p_{T} > 30;Jet #eta;Jet #phi", 75, -2.5, 2.5, 75, -M_PI, M_PI);
+      HJetEtaPhiPT50[iM] = new TH2D(Form("HJetEtaPhiPT50%s", MVALabel[iM].c_str()), "Jet p_{T} > 50;Jet #eta;Jet #phi", 75, -2.5, 2.5, 75, -M_PI, M_PI);
+      HJetEtaPhiPT75[iM] = new TH2D(Form("HJetEtaPhiPT75%s", MVALabel[iM].c_str()), "Jet p_{T} > 75;Jet #eta;Jet #phi", 75, -2.5, 2.5, 75, -M_PI, M_PI);
+      HJetEtaPhiPT100[iM] = new TH2D(Form("HJetEtaPhiPT100%s", MVALabel[iM].c_str()), "Jet p_{T} > 100;Jet #eta;Jet #phi", 75, -2.5, 2.5, 75, -M_PI, M_PI);
+      HJetEtaPhiPT125[iM] = new TH2D(Form("HJetEtaPhiPT125%s", MVALabel[iM].c_str()), "Jet p_{T} > 125;Jet #eta;Jet #phi", 75, -2.5, 2.5, 75, -M_PI, M_PI);
+      HTrackMETX[iM] = new TH1D(Form("HTrackMETX%s", MVALabel[iM].c_str()), ";MET(x);", 150, -500, 500);
+      HTrackMETY[iM] = new TH1D(Form("HTrackMETY%s", MVALabel[iM].c_str()), ";MET(y);", 150, -500, 500);
+      HTrackMET[iM] = new TH1D(Form("HTrackMET%s", MVALabel[iM].c_str()), ";MET;", 150, 0, 700);
+      HPositiveTrackMETX[iM] = new TH1D(Form("HPositiveTrackMETX%s", MVALabel[iM].c_str()), ";MET(x);", 150, -500, 500);
+      HPositiveTrackMETY[iM] = new TH1D(Form("HPositiveTrackMETY%s", MVALabel[iM].c_str()), ";MET(y);", 150, -500, 500);
+      HPositiveTrackMET[iM] = new TH1D(Form("HPositiveTrackMET%s", MVALabel[iM].c_str()), ";MET;", 150, 0, 700);
+      HTrackMETXPeripheral[iM] = new TH1D(Form("HTrackMETXPeripheral%s", MVALabel[iM].c_str()), ";MET(x);", 150, -100, 100);
+      HTrackMETYPeripheral[iM] = new TH1D(Form("HTrackMETYPeripheral%s", MVALabel[iM].c_str()), ";MET(y);", 150, -100, 100);
+      HTrackMETPeripheral[iM] = new TH1D(Form("HTrackMETPeripheral%s", MVALabel[iM].c_str()), ";MET;", 150, 0, 100);
+      HPositiveTrackMETXPeripheral[iM] = new TH1D(Form("HPositiveTrackMETXPeripheral%s", MVALabel[iM].c_str()), ";MET(x);", 150, -100, 100);
+      HPositiveTrackMETYPeripheral[iM] = new TH1D(Form("HPositiveTrackMETYPeripheral%s", MVALabel[iM].c_str()), ";MET(y);", 150, -100, 100);
+      HPositiveTrackMETPeripheral[iM] = new TH1D(Form("HPositiveTrackMETPeripheral%s", MVALabel[iM].c_str()), ";MET;", 150, 0, 100);
+   }
+
+   for(string FileName : InputFileName)
+   {
+      cout << "Processing file " << FileName << endl;
+
+      TFile InputFile(FileName.c_str());
+
+      HiEventTreeMessenger MHiEvent(InputFile);
+      TriggerTreeMessenger MHLT(InputFile);
+      TrackTreeMessenger MTrack(InputFile);
+
+      if(MHiEvent.Tree == nullptr)
+      {
+         InputFile.Close();
+         continue;
+      }
+
+      int EntryCount = MHiEvent.Tree->GetEntries();
+      ProgressBar Bar(cout, EntryCount);
+      Bar.SetStyle(-1);
+
+      for(int iE = 0; iE < EntryCount; iE++)
+      {
+         if(iE < 200 || (iE % (EntryCount / 300)) == 0)
+         {
+            Bar.Update(iE);
+            Bar.Print();
+         }
+
+         MHiEvent.GetEntry(iE);
+         MHLT.GetEntry(iE);
+         MTrack.GetEntry(iE);
+
+         bool GoodLumi = false;
+         if(MHiEvent.Run == 327400 && (MHiEvent.Lumi >= 38 && MHiEvent.Lumi <= 578))
+            GoodLumi = true;
+         if(MHiEvent.Run == 327402 && (MHiEvent.Lumi >= 1 && MHiEvent.Lumi <= 294))
+            GoodLumi = true;
+         if(MHiEvent.Run == 327430 && (MHiEvent.Lumi >= 1 && MHiEvent.Lumi <= 497))
+            GoodLumi = true;
+         if(MHiEvent.Run == 327430 && (MHiEvent.Lumi >= 499 && MHiEvent.Lumi <= 1092))
+            GoodLumi = true;
+         if(MHiEvent.Run == 327462 && (MHiEvent.Lumi >= 42 && MHiEvent.Lumi <= 320))
+            GoodLumi = true;
+         if(MHiEvent.Run == 327524)
+            GoodLumi = true;
+         if(GoodLumi == false)
+            continue;
+
+         HN.Fill(0);
+
+         for(int iM = 0; iM < (int)MVACut.size(); iM++)
+         {
+            vector<PseudoJet> Tracks;
+            FourVector TrackMET;
+            FourVector PositiveTrackMET;
+
+            for(int iT = 0; iT < MTrack.TrackCount; iT++)
+            {
+               if(MTrack.HighPurity[iT] == false)
+                  continue;
+               if(MTrack.TrackAlgo[iT] == 6 && MTrack.TrackMVA[iT] < MVACut[iM])
+                  continue;
+
+               FourVector P;
+               P.SetPtEtaPhi(MTrack.TrackPT[iT], MTrack.TrackEta[iT], MTrack.TrackPhi[iT]);
+               Tracks.push_back(PseudoJet(P[1], P[2], P[3], P[0]));
+
+               TrackMET = TrackMET - P;
+               if(P[3] > 0)
+                  PositiveTrackMET = TrackMET - P;
+            }
+            JetDefinition Definition(antikt_algorithm, 0.4);
+            ClusterSequence Sequence(Tracks, Definition);
+            vector<PseudoJet> FastJets = Sequence.inclusive_jets();
+
+            HTrackMETX[iM]->Fill((TrackMET[1] + TrackMET[2]) / sqrt(2));
+            HTrackMETY[iM]->Fill((TrackMET[2] - TrackMET[1]) / sqrt(2));
+            HTrackMET[iM]->Fill(TrackMET.GetPT());
+            HPositiveTrackMETX[iM]->Fill((PositiveTrackMET[1] + PositiveTrackMET[2]) / sqrt(2));
+            HPositiveTrackMETY[iM]->Fill((PositiveTrackMET[2] - PositiveTrackMET[1]) / sqrt(2));
+            HPositiveTrackMET[iM]->Fill(PositiveTrackMET.GetPT());
+            if(MHiEvent.hiBin > 100)
+            {
+               HTrackMETXPeripheral[iM]->Fill((TrackMET[1] + TrackMET[2]) / sqrt(2));
+               HTrackMETYPeripheral[iM]->Fill((TrackMET[2] - TrackMET[1]) / sqrt(2));
+               HTrackMETPeripheral[iM]->Fill(TrackMET.GetPT());
+               HPositiveTrackMETXPeripheral[iM]->Fill((PositiveTrackMET[1] + PositiveTrackMET[2]) / sqrt(2));
+               HPositiveTrackMETYPeripheral[iM]->Fill((PositiveTrackMET[2] - PositiveTrackMET[1]) / sqrt(2));
+               HPositiveTrackMETPeripheral[iM]->Fill(PositiveTrackMET.GetPT());
+            }
+
+            vector<FourVector> Jets;
+            for(PseudoJet J : FastJets)
+               Jets.push_back(FourVector(J.e(), J.px(), J.py(), J.pz()));
+
+            int JetCount = 0;
+            int JetCountPT30 = 0;
+            int JetCountPT50 = 0;
+            int JetCountPT75 = 0;
+            int JetCountPT100 = 0;
+            int JetCountPT125 = 0;
+
+            if(Jets.size() > 0)
+               HLeadingJetPT[iM]->Fill(Jets[0].GetPT());
+            for(FourVector J : Jets)
+            {
+               JetCount = JetCount + 1;
+               if(J.GetPT() > 30)    JetCountPT30 = JetCountPT30 + 1;
+               if(J.GetPT() > 50)    JetCountPT50 = JetCountPT50 + 1;
+               if(J.GetPT() > 75)    JetCountPT75 = JetCountPT75 + 1;
+               if(J.GetPT() > 100)   JetCountPT100 = JetCountPT100 + 1;
+               if(J.GetPT() > 125)   JetCountPT125 = JetCountPT125 + 1;
+
+               HJetPT[iM]->Fill(J.GetPT());
+               if(J.GetEta() > -2.5 && J.GetEta() < -1.5)
+                  HJetPTEtaN25N15[iM]->Fill(J.GetPT());
+               if(J.GetEta() > -1.5 && J.GetEta() < 0.0)
+                  HJetPTEtaN15N00[iM]->Fill(J.GetPT());
+               if(J.GetEta() > 0.0 && J.GetEta() < 1.5)
+                  HJetPTEtaP00P15[iM]->Fill(J.GetPT());
+               if(J.GetEta() > 1.5 && J.GetEta() < 2.5)
+                  HJetPTEtaP15P25[iM]->Fill(J.GetPT());
+
+               if(J.GetPT() > 30)
+                  HJetEtaPhiPT30[iM]->Fill(J.GetEta(), J.GetPhi());
+               if(J.GetPT() > 50)
+                  HJetEtaPhiPT50[iM]->Fill(J.GetEta(), J.GetPhi());
+               if(J.GetPT() > 75)
+                  HJetEtaPhiPT75[iM]->Fill(J.GetEta(), J.GetPhi());
+               if(J.GetPT() > 100)
+                  HJetEtaPhiPT100[iM]->Fill(J.GetEta(), J.GetPhi());
+               if(J.GetPT() > 125)
+                  HJetEtaPhiPT125[iM]->Fill(J.GetEta(), J.GetPhi());
+            }
+
+            HJetCount[iM]->Fill(JetCount);
+            HJetCountPT30[iM]->Fill(JetCountPT30);
+            HJetCountPT50[iM]->Fill(JetCountPT50);
+            HJetCountPT75[iM]->Fill(JetCountPT75);
+            HJetCountPT100[iM]->Fill(JetCountPT100);
+            HJetCountPT125[iM]->Fill(JetCountPT125);
+         }
+      }
+
+      Bar.Update(EntryCount);
+      Bar.Print();
+      Bar.PrintLine();
+
+      InputFile.Close();
+   }
+
+   OutputFile.cd();
+
+   HN.Write();
+   
+   for(auto H : HJetCount)                      if(H != nullptr)   H->Write();
+   for(auto H : HJetCountPT30)                  if(H != nullptr)   H->Write();
+   for(auto H : HJetCountPT50)                  if(H != nullptr)   H->Write();
+   for(auto H : HJetCountPT75)                  if(H != nullptr)   H->Write();
+   for(auto H : HJetCountPT100)                 if(H != nullptr)   H->Write();
+   for(auto H : HJetCountPT125)                 if(H != nullptr)   H->Write();
+   for(auto H : HJetPT)                         if(H != nullptr)   H->Write();
+   for(auto H : HLeadingJetPT)                  if(H != nullptr)   H->Write();
+   for(auto H : HJetPTEtaN25N15)                if(H != nullptr)   H->Write();
+   for(auto H : HJetPTEtaN15N00)                if(H != nullptr)   H->Write();
+   for(auto H : HJetPTEtaP00P15)                if(H != nullptr)   H->Write();
+   for(auto H : HJetPTEtaP15P25)                if(H != nullptr)   H->Write();
+   for(auto H : HJetEtaPhiPT30)                 if(H != nullptr)   H->Write();
+   for(auto H : HJetEtaPhiPT50)                 if(H != nullptr)   H->Write();
+   for(auto H : HJetEtaPhiPT75)                 if(H != nullptr)   H->Write();
+   for(auto H : HJetEtaPhiPT100)                if(H != nullptr)   H->Write();
+   for(auto H : HJetEtaPhiPT125)                if(H != nullptr)   H->Write();
+   for(auto H : HTrackMETX)                     if(H != nullptr)   H->Write();
+   for(auto H : HTrackMETY)                     if(H != nullptr)   H->Write();
+   for(auto H : HTrackMET)                      if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMETX)             if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMETY)             if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMET)              if(H != nullptr)   H->Write();
+   for(auto H : HTrackMETXPeripheral)           if(H != nullptr)   H->Write();
+   for(auto H : HTrackMETYPeripheral)           if(H != nullptr)   H->Write();
+   for(auto H : HTrackMETPeripheral)            if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMETXPeripheral)   if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMETYPeripheral)   if(H != nullptr)   H->Write();
+   for(auto H : HPositiveTrackMETPeripheral)    if(H != nullptr)   H->Write();
+   
+   OutputFile.Close();
+
+   return 0;
+}
+
+
+

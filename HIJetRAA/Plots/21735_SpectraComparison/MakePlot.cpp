87a94d6f35598a81741823f7a0fdb656ec8f64d8
FHead <chen.yi.first@gmail.com>
2019-10-25 21:23:52 +0200

    small change to Spectra comparison code

--- HIJetRAA/Plots/21735_SpectraComparison/MakePlot.cpp
+++ HIJetRAA/Plots/21735_SpectraComparison/MakePlot.cpp
@@ -192,10 +192,10 @@ int main(int argc, char *argv[])
    Legend.Draw();
    P2.cd();
    HWorldR.Draw("axis");
+   Line.Draw("l");
    GRatioIan.Draw("2");
    GRatio.Draw("p"); 
    GRatioSys.Draw("2");
-   Line.Draw("l");
 
    string OutputBase = "SpectraSinglePanelR04";
 

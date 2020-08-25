032e8e6e36beaf9d0ff4299bb0b6770a062eec78
FHead <chen.yi.first@gmail.com>
2020-08-25 15:32:24 +0200

    22652 sync

---
+++ JetScape/BayesianAnalysis/22623_BeautifulPlots/MakePosteriorPlot.cpp
@@ -0,0 +1,345 @@
+#include <iostream>
+using namespace std;
+
+#include "TSpline.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TGraphErrors.h"
+#include "TGaxis.h"
+#include "TExec.h"
+#include "TLatex.h"
+#include "TLegend.h"
+
+#include "SetStyle.h"
+#include "CommandLine.h"
+#include "PlotHelper4.h"
+
+int main(int argc, char *argv[]);
+void SetPad(TPad &P);
+void SetAxis(TGaxis &A);
+void Set2DHistogram(TH2D *H);
+void SetGraph(TGraphErrors *G, bool Central);
+void SetMedianGraph(TGraphErrors *G, bool Central);
+void Transcribe(TSpline3 *L, TGraphErrors *G, double MinX, double MaxX);
+
+int main(int argc, char *argv[])
+{
+   SetThesisStyle();
+
+   CommandLine CL(argc, argv);
+
+   string FileName       = CL.Get("file");
+   bool IsPrior          = CL.GetBool("prior", false);
+   string Output         = CL.Get("output");
+
+   string Type = IsPrior ? "Prior" : "Posterior";
+   string TypeString = IsPrior ? "Design" : "Posterior";
+
+   TFile File(FileName.c_str());
+
+   double PaddingLeft = 100;
+   double PaddingRight = 50;
+   double PaddingTop = 75;
+   double PaddingBottom = 100;
+
+   double PanelWidth = 500;
+   double PanelHeight = 500;
+
+   double CanvasWidth = PaddingLeft + PaddingRight + PanelWidth * 3;
+   double CanvasHeight = PaddingTop + PaddingBottom + PanelHeight;
+
+   double PadX0 = PaddingLeft / CanvasWidth;
+   double PadX = PanelWidth / CanvasWidth;
+   double PadY0 = PaddingBottom / CanvasHeight;
+   double PadY = PanelHeight / CanvasHeight;
+   double PadTop = PaddingTop / CanvasHeight;
+
+   TCanvas Canvas("Canvas", "", CanvasWidth, CanvasHeight);
+
+   TPad Pad1("Pad1", "", PadX0 + PadX * 0, PadY0, PadX0 + PadX * 1, PadY0 + PadY, 0);
+   TPad Pad2("Pad1", "", PadX0 + PadX * 1, PadY0, PadX0 + PadX * 2, PadY0 + PadY, 0);
+   TPad Pad3("Pad1", "", PadX0 + PadX * 2, PadY0, PadX0 + PadX * 3, PadY0 + PadY, 0);
+
+   SetPad(Pad1);
+   SetPad(Pad2);
+   SetPad(Pad3);
+
+   TH2D *H0 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 0));
+   TH2D *H1 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 1));
+   TH2D *H2 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 2));
+   TH2D *H3 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 3));
+   TH2D *H4 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 4));
+   TH2D *H5 = (TH2D *)File.Get(Form("HRAA%s%d", Type.c_str(), 5));
+
+   Set2DHistogram(H0);
+   Set2DHistogram(H1);
+   Set2DHistogram(H2);
+   Set2DHistogram(H3);
+   Set2DHistogram(H4);
+   Set2DHistogram(H5);
+
+   double MinX0 = H0->GetXaxis()->GetBinLowEdge(1) * 1.000;
+   double MinX2 = H2->GetXaxis()->GetBinLowEdge(1) * 1.001;
+   double MinX4 = H4->GetXaxis()->GetBinLowEdge(1) * 1.001;
+   double MaxX0 = H0->GetXaxis()->GetBinUpEdge(H0->GetNbinsX()) * 0.999;
+   double MaxX2 = H2->GetXaxis()->GetBinUpEdge(H0->GetNbinsX()) * 0.999;
+   double MaxX4 = H4->GetXaxis()->GetBinUpEdge(H0->GetNbinsX()) * 1.000;
+   double MinY = H0->GetYaxis()->GetBinLowEdge(1);
+   double MaxY = H0->GetYaxis()->GetBinUpEdge(H0->GetNbinsY());
+
+   TGaxis AxisY(PadX0, PadY0, PadX0, PadY0 + PadY, MinY, MaxY, 510, "S");
+   TGaxis AxisX1(PadX0 + PadX * 0, PadY0, PadX0 + PadX * 1, PadY0, MinX0, MaxX0, 510, "S");
+   TGaxis AxisX2(PadX0 + PadX * 1, PadY0, PadX0 + PadX * 2, PadY0, MinX2, MaxX2, 510, "S");
+   TGaxis AxisX3(PadX0 + PadX * 2, PadY0, PadX0 + PadX * 3, PadY0, MinX4, MaxX4, 510, "S");
+
+   SetAxis(AxisY);
+   SetAxis(AxisX1);
+   SetAxis(AxisX2);
+   SetAxis(AxisX3);
+
+   TGraphErrors *D0 = (TGraphErrors *)File.Get("Data0");
+   TGraphErrors *D1 = (TGraphErrors *)File.Get("Data1");
+   TGraphErrors *D2 = (TGraphErrors *)File.Get("Data2");
+   TGraphErrors *D3 = (TGraphErrors *)File.Get("Data3");
+   TGraphErrors *D4 = (TGraphErrors *)File.Get("Data4");
+   TGraphErrors *D5 = (TGraphErrors *)File.Get("Data5");
+   TGraphErrors *S0 = (TGraphErrors *)File.Get("DataS0");
+   TGraphErrors *S1 = (TGraphErrors *)File.Get("DataS1");
+   TGraphErrors *S2 = (TGraphErrors *)File.Get("DataS2");
+   TGraphErrors *S3 = (TGraphErrors *)File.Get("DataS3");
+   TGraphErrors *S4 = (TGraphErrors *)File.Get("DataS4");
+   TGraphErrors *S5 = (TGraphErrors *)File.Get("DataS5");
+
+   SetGraph(D0, true);
+   SetGraph(D1, false);
+   SetGraph(D2, true);
+   SetGraph(D3, false);
+   SetGraph(D4, true);
+   SetGraph(D5, false);
+   SetGraph(S0, true);
+   SetGraph(S1, false);
+   SetGraph(S2, true);
+   SetGraph(S3, false);
+   SetGraph(S4, true);
+   SetGraph(S5, false);
+
+   TSpline3 *MS0 = (TSpline3 *)File.Get("Median0");
+   TSpline3 *MS1 = (TSpline3 *)File.Get("Median1");
+   TSpline3 *MS2 = (TSpline3 *)File.Get("Median2");
+   TSpline3 *MS3 = (TSpline3 *)File.Get("Median3");
+   TSpline3 *MS4 = (TSpline3 *)File.Get("Median4");
+   TSpline3 *MS5 = (TSpline3 *)File.Get("Median5");
+
+   TGraphErrors M0;   Transcribe(MS0, &M0, MinX0, MaxX0);
+   TGraphErrors M1;   Transcribe(MS1, &M1, MinX0, MaxX0);
+   TGraphErrors M2;   Transcribe(MS2, &M2, MinX2, MaxX2);
+   TGraphErrors M3;   Transcribe(MS3, &M3, MinX2, MaxX2);
+   TGraphErrors M4;   Transcribe(MS4, &M4, MinX4, MaxX4);
+   TGraphErrors M5;   Transcribe(MS5, &M5, MinX4, MaxX4);
+
+   SetMedianGraph(&M0, true);
+   SetMedianGraph(&M1, false);
+   SetMedianGraph(&M2, true);
+   SetMedianGraph(&M3, false);
+   SetMedianGraph(&M4, true);
+   SetMedianGraph(&M5, false);
+
+   TGraphErrors ML;   // for legend
+   SetMedianGraph(&ML, true);
+   ML.SetLineColor(kBlack);
+
+   TLegend Legend1(0.075, 0.575, 0.375, 0.775);
+   Legend1.SetBorderSize(0);
+   Legend1.SetTextFont(42);
+   Legend1.SetTextSize(0.060);
+   Legend1.SetFillStyle(0);
+   Legend1.AddEntry(D0, "0-10% Centrality", "plf");
+   Legend1.AddEntry(D1, "40-50% Centrality", "plf");
+   if(IsPrior == false)   Legend1.AddEntry(&ML, "Median prediction", "l");
+   else                   Legend1.AddEntry("", "", "");
+
+   TLegend Legend2(0.075, 0.575, 0.375, 0.775);
+   Legend2.SetBorderSize(0);
+   Legend2.SetTextFont(42);
+   Legend2.SetTextSize(0.060);
+   Legend2.SetFillStyle(0);
+   Legend2.AddEntry(D2, "0-5% Centrality", "plf");
+   Legend2.AddEntry(D3, "30-40% Centrality", "plf");
+   if(IsPrior == false)   Legend2.AddEntry(&ML, "Median prediction", "l");
+   else                   Legend2.AddEntry("", "", "");
+
+   TLegend Legend3(0.075, 0.575, 0.375, 0.775);
+   Legend3.SetBorderSize(0);
+   Legend3.SetTextFont(42);
+   Legend3.SetTextSize(0.060);
+   Legend3.SetFillStyle(0);
+   Legend3.AddEntry(D4, "0-10% Centrality", "plf");
+   Legend3.AddEntry(D5, "30-50% Centrality", "plf");
+   if(IsPrior == false)   Legend3.AddEntry(&ML, "Median prediction", "l");
+   else                   Legend3.AddEntry("", "", "");
+
+   TLatex Latex;
+   Latex.SetNDC();
+   Latex.SetTextSize(0.060);
+   Latex.SetTextFont(42);
+   Latex.SetTextAlign(22);
+
+   Latex.SetTextAngle(90);
+   Latex.DrawLatex(PadX0 * 0.3, PadY0 + PadY * 0.5, "R_{AA}");
+   Latex.SetTextAngle(0);
+   Latex.DrawLatex(PadX0 + PadX * 0.5, PadY0 * 0.3, "p_{T} (GeV)");
+   Latex.DrawLatex(PadX0 + PadX * 1.5, PadY0 * 0.3, "p_{T} (GeV)");
+   Latex.DrawLatex(PadX0 + PadX * 2.5, PadY0 * 0.3, "p_{T} (GeV)");
+
+   Latex.SetTextAlign(33);
+   Latex.DrawLatex(PadX0 + PadX * 3, PadY0 + PadY + PadTop * 0.5, "#font[62]{JETSCAPE}");
+
+   TExec Exe1("Exe1", "double S1[3] = {0.000, 0.200, 1.000}; double R1[3] = {1.000, 0.300, 0.200}; double G1[3] = {1.000, 1.000, 1.000}; double B1[3] = {1.000, 1.000, 1.000}; TColor::CreateGradientColorTable(3, S1, R1, G1, B1, 99, 1.0);");
+   TExec Exe2("Exe2", "double S2[3] = {0.000, 0.200, 1.000}; double R2[3] = {1.000, 1.000, 1.000}; double G2[3] = {1.000, 0.300, 0.200}; double B2[3] = {1.000, 0.300, 0.200}; TColor::CreateGradientColorTable(3, S2, R2, G2, B2, 99, 0.4);");
+
+   Pad1.cd();
+   H0->Draw("axis");
+   Exe1.Draw();
+   H0->Draw("col same");
+   Exe2.Draw();
+   H1->Draw("col same");
+   if(IsPrior == false)   M0.Draw("l");
+   if(IsPrior == false)   M1.Draw("l");
+   D0->Draw("p");
+   D1->Draw("p");
+   S0->Draw("[]");
+   S1->Draw("[]");
+   H0->Draw("axis same");
+   Legend1.Draw();
+
+   Latex.SetTextAlign(13);
+   Latex.DrawLatex(0.075, 0.925, ("AuAu 200 GeV " + TypeString).c_str());
+   Latex.DrawLatex(0.075, 0.850, "Data from PHENIX");
+
+   Pad2.cd();
+   H2->Draw("axis");
+   Exe1.Draw();
+   H2->Draw("col same");
+   Exe2.Draw();
+   H3->Draw("col same");
+   if(IsPrior == false)   M2.Draw("l");
+   if(IsPrior == false)   M3.Draw("l");
+   D2->Draw("p");
+   D3->Draw("p");
+   S2->Draw("[]");
+   S3->Draw("[]");
+   H2->Draw("axis same");
+   Legend2.Draw();
+   
+   Latex.SetTextAlign(13);
+   Latex.DrawLatex(0.075, 0.925, ("PbPb 2.76 TeV " + TypeString).c_str());
+   Latex.DrawLatex(0.075, 0.850, "Data from ATLAS");
+
+   Pad3.cd();
+   H4->Draw("axis");
+   Exe1.Draw();
+   H4->Draw("col same");
+   Exe2.Draw();
+   H5->Draw("col same");
+   if(IsPrior == false)   M4.Draw("l");
+   if(IsPrior == false)   M5.Draw("l");
+   D4->Draw("p");
+   D5->Draw("p");
+   S4->Draw("[]");
+   S5->Draw("[]");
+   H4->Draw("axis same");
+   Legend3.Draw();
+   
+   Latex.SetTextAlign(13);
+   Latex.DrawLatex(0.075, 0.925, ("PbPb 5.02 TeV " + TypeString).c_str());
+   Latex.DrawLatex(0.075, 0.850, "Data from CMS");
+
+   Canvas.SaveAs((Output + ".pdf").c_str());
+   Canvas.SaveAs((Output + ".png").c_str());
+}
+
+void SetPad(TPad &P)
+{
+   P.SetLeftMargin(0);
+   P.SetTopMargin(0);
+   P.SetRightMargin(0);
+   P.SetBottomMargin(0);
+   P.Draw();
+}
+
+void SetAxis(TGaxis &A)
+{
+   A.SetLabelFont(42);
+   A.SetLabelSize(0.050);
+   A.SetMaxDigits(6);
+   A.SetTickLength(0);
+   A.Draw();
+}
+
+void Set2DHistogram(TH2D *H)
+{
+   H->SetStats(0);
+   
+   for(int iX = 1; iX <= H->GetNbinsX(); iX++)
+      for(int iY = 1; iY <= H->GetNbinsX(); iY++)
+         H->SetBinContent(iX, iY, H->GetBinContent(iX, iY) + 1e-10);
+}
+
+void SetGraph(TGraphErrors *G, bool Central)
+{
+   G->SetMarkerStyle(20);
+   G->SetMarkerSize(1.5);
+   G->SetLineWidth(2);
+
+   if(Central)
+   {
+      G->SetLineColor(kBlue + 3);
+      G->SetMarkerColor(kBlue + 3);
+      G->SetFillColor(kCyan);
+   }
+   else
+   {
+      G->SetLineColor(kRed + 3);
+      G->SetMarkerColor(kRed + 3);
+      G->SetFillColor(kPink);
+   }
+
+   for(int i = 0; i < G->GetN(); i++)
+   {
+      double X, Y;
+      G->GetPoint(i, X, Y);
+
+      if(Central == true)
+         X = X * 1.0025;
+      else
+         X = X * 0.9975;
+
+      G->SetPoint(i, X, Y);
+   }
+}
+
+void SetMedianGraph(TGraphErrors *G, bool Central)
+{
+   SetGraph(G, Central);
+   G->SetLineStyle(kDashed);
+}
+
+void Transcribe(TSpline3 *L, TGraphErrors *G, double MinX, double MaxX)
+{
+   double DeltaX = MaxX - MinX;
+   MinX = MinX - DeltaX * 0.1;
+   MaxX = MaxX + DeltaX * 0.1;
+
+   int N = 1000;
+
+   for(int i = 0; i < N; i++)
+   {
+      double X = MinX + (MaxX - MinX) / N * (i + 0.5);
+      double Y = L->Eval(X);
+      G->SetPoint(i, X, Y);
+   }
+}
+
+
---
+++ JetScape/BayesianAnalysis/22623_BeautifulPlots/MakeScatterPlot2.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TPad.h"
+#include "TGaxis.h"
+#include "TLatex.h"
+#include "TLegend.h"
+
+#include "CommandLine.h"
+#include "PlotHelper4.h"
+#include "SetStyle.h"
+#include "TExec.h"
+
+int main(int argc, char *argv[]);
+void SetPad(TPad *P);
+void SetAxis(TGaxis *A);
+void Set1DHistogram(TH1D *H, int Color);
+void Set2DHistogram(TH2D *H, int Color);
+void Block2DHistogram(TH2D *H, int N, int Offset);
+
+int main(int argc, char *argv[])
+{
+   SetThesisStyle();
+
+   // gStyle->SetPalette(51);
+
+   CommandLine CL(argc, argv);
+
+   vector<string> FileNames = CL.GetStringVector("file");
+   vector<string> Labels    = CL.GetStringVector("label");
+   vector<string> Name      = CL.GetStringVector("name", {"A", "B", "C", "D"});
+   vector<string> Details   = CL.GetStringVector("detail", vector<string>({"", ""}));
+   bool DoDots              = CL.GetBool("dot", false);
+   double LegendX           = CL.GetDouble("legendx", 0.7);
+   double LegendY           = CL.GetDouble("legendy", 0.7);
+   int LowerIndex           = CL.GetInt("lowerindex", 0);
+   int UpperIndex           = CL.GetInt("upperindex", 1);
+   string Output            = CL.Get("output", "Scatter.pdf");
+
+   int Colors[] = {kRed, kBlue, kGreen + 3};
+   TExec Exe1("Exe1", "double S1[2] = {0.000, 1.000}; double R1[2] = {1.000, 1.000}; double G1[2] = {1.000, 0.100}; double B1[2] = {1.000, 0.100}; TColor::CreateGradientColorTable(2, S1, R1, G1, B1, 99, 1.0);"); 
+   TExec Exe2("Exe2", "double S1[2] = {0.000, 1.000}; double R1[2] = {1.000, 0.100}; double G1[2] = {1.000, 0.100}; double B1[2] = {1.000, 1.000}; TColor::CreateGradientColorTable(2, S1, R1, G1, B1, 99, 1.0);"); 
+   TExec Exe3("Exe3", "double S1[2] = {0.000, 1.000}; double R1[2] = {1.000, 0.100}; double G1[2] = {1.000, 1.000}; double B1[2] = {1.000, 0.100}; TColor::CreateGradientColorTable(2, S1, R1, G1, B1, 99, 1.0);"); 
+
+   int NFiles = FileNames.size();
+   int N = Name.size();
+
+   vector<TFile *> Files;
+   for(int i = 0; i < NFiles; i++)
+      Files.push_back(new TFile(FileNames[i].c_str()));
+
+   vector<double> Min, Max;
+   for(int i = 0; i < N; i++)
+   {
+      TH1D *H = (TH1D *)Files[0]->Get(Form("HPosterior%d", i));
+
+      Min.push_back(H->GetXaxis()->GetBinLowEdge(1));
+      Max.push_back(H->GetXaxis()->GetBinUpEdge(H->GetNbinsX()));
+   }
+
+   double PaddingLeft = 175;
+   double PaddingBottom = 175;
+   double PaddingRight = 50;
+   double PaddingTop = 50;
+   double PanelSizeX = 500;
+   double PanelSizeY = 500;
+
+   double CanvasX = PaddingLeft + PaddingRight + PanelSizeX * N;
+   double CanvasY = PaddingTop + PaddingBottom + PanelSizeY * N;
+   
+   double PadX0 = PaddingLeft / CanvasX;
+   double PadY0 = PaddingBottom / CanvasY;
+   double PadSizeX = PanelSizeX / CanvasX;
+   double PadSizeY = PanelSizeY / CanvasY;
+
+   TCanvas Canvas("Canvas", "", CanvasX, CanvasY);
+
+   TLatex Latex;
+   Latex.SetTextFont(62);
+   Latex.SetTextSize(0.03);
+   Latex.SetTextAlign(22);
+   Latex.SetNDC();
+
+   for(int i = 0; i < N; i++)
+   {
+      Latex.DrawLatex(PadX0 + PadSizeX * (i + 0.5), PadY0 * 0.3, Name[i].c_str());
+      Latex.DrawLatex(PadX0 * 0.3, PadY0 + PadSizeY * (N - i - 0.5), Name[i].c_str());
+   }
+
+   double LineHeight = 0.05;
+
+   Latex.SetTextFont(42);
+   Latex.SetTextSize(0.04);
+   Latex.SetTextAlign(33);
+   Latex.DrawLatex(PadX0 + PadSizeX * N, PadY0 + PadSizeY * N, "#font[62]{JETSCAPE}");
+
+   for(int i = 0; i < (int)Details.size(); i++)
+      Latex.DrawLatex(PadX0 + PadSizeX * N, PadY0 + PadSizeY * N - LineHeight * (i + 1), Details[i].c_str());
+   
+   vector<TGaxis *> XAxis, YAxis;
+
+   for(int i = 0; i < N; i++)
+   {
+      double Protector = (i == N - 1) ? 1 : 0.999;
+
+      XAxis.push_back(new TGaxis(PadX0 + PadSizeX * i, PadY0, PadX0 + PadSizeX * (i + 1), PadY0, Min[i], Max[i] * Protector, 305, ""));
+      SetAxis(XAxis[i]);
+      XAxis[i]->Draw();
+      
+      Protector = (i == 1) ? 1 : 0.999;
+      
+      YAxis.push_back(new TGaxis(PadX0, PadY0 + PadSizeY * (N - i - 1), PadX0, PadY0 + PadSizeY * (N - i), Min[i], Max[i] * Protector, 305, ""));
+      SetAxis(YAxis[i]);
+      if(i != 0)
+         YAxis[i]->Draw();
+   }
+
+   vector<vector<TPad *>> Pads;
+   for(int i = 0; i < N; i++)
+   {
+      Pads.push_back(vector<TPad *>());
+      for(int j = 0; j < N; j++)
+      {
+         Pads[i].push_back(new TPad(Form("P%d", i), "",
+            PadX0 + PadSizeX * i,       PadY0 + PadSizeY * (N - j - 1),
+            PadX0 + PadSizeX * (i + 1), PadY0 + PadSizeY * (N - j), 0));
+         SetPad(Pads[i][j]);
+      }
+   }
+
+   TLegend Legend(LegendX + 0.000, LegendY + 0.000,
+                  LegendX + 0.250, LegendY + 0.100);
+   Legend.SetTextFont(42);
+   Legend.SetTextSize(0.035);
+   Legend.SetFillStyle(0);
+   Legend.SetBorderSize(0);
+
+   for(int i = 0; i < N; i++)
+   {
+      Pads[i][i]->cd();
+      double Max = 0;
+      for(int j = 0; j < NFiles; j++)
+      {
+         TH1D *H = (TH1D *)Files[j]->Get(Form("HPosterior%d", i));
+         Set1DHistogram(H, Colors[j]);
+         Max = max(Max, H->GetMaximum());
+         if(i == 0)
+            Legend.AddEntry(H, Labels[j].c_str(), "lf");
+      }
+      for(int j = 0; j < NFiles; j++)
+      {
+         TH1D *H = (TH1D *)Files[j]->Get(Form("HPosterior%d", i));
+         H->SetMaximum(Max * 1.2);
+         H->Draw(j == 0 ? "hist" : "hist same");
+      }
+   }
+   for(int i = 0; i < N; i++)
+   {
+      for(int j = 0; j < N; j++)
+      {
+         if(i == j)
+            continue;
+
+         Pads[i][j]->cd();
+
+         if(i < j)   // plot from first file
+         {
+            if(Files.size() > LowerIndex)
+            {
+               TH2D *H = (TH2D *)Files[LowerIndex]->Get(Form("HPosterior%d%d", i, j));
+               cout << i << " " << j << " " << H << endl;
+               Set2DHistogram(H, Colors[LowerIndex]);
+               // Block2DHistogram(H, NFiles, LowerIndex);
+
+               H->Draw("axis");
+               if(LowerIndex == 0)        Exe1.Draw();
+               else if(LowerIndex == 1)   Exe2.Draw();
+               else                       Exe3.Draw();
+               H->Draw("col same");
+               H->Draw("axis same");
+            }
+         }
+         else   // plot from the second file...
+         {
+            if(Files.size() > UpperIndex)   // ...if there is indeed a file
+            {
+               TH2D *H = (TH2D *)Files[UpperIndex]->Get(Form("HPosterior%d%d", i, j));
+               cout << i << " " << j << " " << H << endl;
+               Set2DHistogram(H, Colors[UpperIndex]);
+               // Block2DHistogram(H, NFiles, UpperIndex);
+
+               H->Draw("axis");
+               if(UpperIndex == 0)        Exe1.Draw();
+               else if(UpperIndex == 1)   Exe2.Draw();
+               else                       Exe3.Draw();
+               H->Draw("col same");
+               H->Draw("axis same");
+            }
+         }
+      }
+   }
+
+   Canvas.cd();
+   if(NFiles > 1)
+      Legend.Draw();
+
+   Canvas.SaveAs((Output + ".pdf").c_str());
+   Canvas.SaveAs((Output + ".png").c_str());
+
+   for(int i = 0; i < N; i++)
+      for(int j = 0; j < N; j++)
+         if(Pads[i][j] != nullptr)
+            delete Pads[i][j];
+   
+   for(int i = 0; i < N; i++)
+   {
+      delete XAxis[i];
+      delete YAxis[i];
+   }
+
+   for(int i = 0; i < NFiles; i++)
+   {
+      Files[i]->Close();
+      delete Files[i];
+   }
+
+   return 0;
+}
+
+void SetPad(TPad *P)
+{
+   P->SetLeftMargin(0);
+   P->SetTopMargin(0);
+   P->SetRightMargin(0);
+   P->SetBottomMargin(0);
+   // P->SetTickx();
+   // P->SetTicky();
+   P->Draw();
+}
+
+void SetAxis(TGaxis *A)
+{
+   A->SetLabelFont(42);
+   A->SetLabelSize(0.030);
+   A->SetMaxDigits(6);
+}
+
+void Set1DHistogram(TH1D *H, int Color)
+{
+   H->SetStats(0);
+   H->SetLineColor(Color);
+   // H->SetFillColor(Color);
+   H->SetLineWidth(2);
+   H->GetXaxis()->SetNdivisions(305);
+   H->SetTickLength(0, "y");
+   H->Scale(1 / H->Integral());
+}
+
+void Set2DHistogram(TH2D *H, int Color)
+{
+   H->SetStats(0);
+   H->GetXaxis()->SetNdivisions(305);
+   H->GetYaxis()->SetNdivisions(305);
+   H->SetMarkerColor(Color);
+   H->SetMarkerStyle(1);
+
+   for(int iX = 1; iX <= H->GetNbinsX(); iX++)
+      for(int iY = 1; iY <= H->GetNbinsX(); iY++)
+         H->SetBinContent(iX, iY, H->GetBinContent(iX, iY) + 1e-10);
+
+   // H->Rebin2D(2, 2);
+
+   // H->SetTickLength(0, "y");
+   // H->SetTickLength(0, "x");
+}
+
+void Block2DHistogram(TH2D *H, int N, int Offset)
+{
+   for(int i = 1; i <= H->GetNbinsX(); i++)
+   {
+      for(int j = 1; j <= H->GetNbinsY(); j++)
+      {
+         if((i + j) % N != Offset)
+            H->SetBinContent(i, j, 0);
+      }
+   }
+}
+

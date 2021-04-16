d12f95711b1b3939961abe3ea74a4f23a26540c6
FHead <chen.yi.first@gmail.com>
2021-04-16 18:46:45 +0200

    Pretty D plot

--- MachineLearning/ALEPHGAN/23435_PrettyDPlot/MakePlot.cpp
+++ MachineLearning/ALEPHGAN/23435_PrettyDPlot/MakePlot.cpp
@@ -9,16 +9,31 @@ using namespace std;
 #include "TLatex.h"
 
 #include "SetStyle.h"
+#include "CommandLine.h"
 
 int main(int argc, char *argv[])
 {
    SetThumbStyle();
    vector<int> Colors = GetPrimaryColors();
 
-   TFile File("Meow.root");
+   CommandLine CL(argc, argv);
 
-   TH1D *HBase = (TH1D *)File.Get("HD1OppositeBase");
-   TH1D *HVariant = (TH1D *)File.Get("HD1OppositeVariant");
+   string InputFileName = CL.Get("Input");
+   string OutputBase    = CL.Get("Output");
+   string BaseName      = CL.Get("Base");
+   string VariantName   = CL.Get("Variant");
+   string XTitle        = CL.Get("XTitle", "D^{0} Candidate Mass [GeV/c^{2}]");
+   string YTitle        = CL.Get("YTitle", "Entries / (5 MeV/c^{2})");
+   double XMin          = CL.GetDouble("XMin");
+   double XMax          = CL.GetDouble("XMax");
+   double YMin          = CL.GetDouble("YMin");
+   double YMax          = CL.GetDouble("YMax");
+   string ExtraInfo     = CL.Get("ExtraInfo", "");
+
+   TFile File(InputFileName.c_str());
+
+   TH1D *HBase = (TH1D *)File.Get(BaseName.c_str());
+   TH1D *HVariant = (TH1D *)File.Get(VariantName.c_str());
 
    HBase->SetMarkerColor(Colors[1]);
    HBase->SetMarkerSize(2.25);
@@ -34,10 +49,11 @@ int main(int argc, char *argv[])
 
    TCanvas Canvas;
 
-   TH2D HWorld("HWorld",
-      ";D^{0} Candidate Mass [GeV/c^{2}];Entries / (5 MeV/c^{2})",
-      100, 1.75, 2.0, 100, 13.1, 18.9);
+   TH2D HWorld("HWorld", "", 100, XMin, XMax, 100, YMin, YMax);
    HWorld.SetStats(0);
+   HWorld.SetTitle("");
+   HWorld.GetXaxis()->SetTitle(XTitle.c_str());
+   HWorld.GetYaxis()->SetTitle(YTitle.c_str());
 
    HWorld.Draw("axis");
    HBase->Draw("same");
@@ -57,10 +73,10 @@ int main(int argc, char *argv[])
    Latex.SetTextSize(0.05);
    Latex.SetTextAlign(12);
    Latex.SetNDC();
-   Latex.DrawLatex(0.25, 0.25, "D^{0} p > 1 GeV/c");
+   Latex.DrawLatex(0.25, 0.25, ExtraInfo.c_str());
 
-   Canvas.SaveAs("DMass.pdf");
-   Canvas.SaveAs("DMass.C");
+   Canvas.SaveAs(Form("%s.pdf", OutputBase.c_str()));
+   Canvas.SaveAs(Form("%s.C", OutputBase.c_str()));
 
    File.Close();
 

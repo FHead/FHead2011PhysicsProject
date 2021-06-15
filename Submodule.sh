5c1d12618d0681f90a15837f81ca6b446065bbde
FHead <chen.yi.first@gmail.com>
2021-06-15 20:41:20 +0200

    submodule dangerous script

---
+++ Submodule.sh
@@ -0,0 +1,30 @@
+
+path=$1
+url=$2
+
+# split the "main repo"
+git subtree split -P $1 -b NewBranch$1
+
+# Create your repository, and get git url
+
+# add remote for branch
+git remote add submodule $2
+
+# push the submodule
+git push -u submodule NewBranch$1:master
+
+# remove path
+git rm -r $1
+
+# Stage and commit changes
+git add -A $1
+git commit -m "Remove $1 for submodule replacement"
+
+# add the submodule
+git submodule add $2 $1
+
+# and once your submodule is added commit the .gitmodules file
+git add .gitmodules
+git commit -m "Added $1 as submodule"
+
+

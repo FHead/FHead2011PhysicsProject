d8b32e4d250d8370f51d2bb95041b0f03fbd67c4
FHead <chen.yi.first@gmail.com>
2021-06-16 11:15:58 +0200

    Remove HIJetRAA for submodule replacement

--- HIJetRAA/Plots/21665_ExtractPlots/TranscribeAll.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p root
-for i in `ls 20191120 | grep C$`
-do
-   echo Processing $i
-   root -l -b -q 20191120/${i/.C}.C "Transcribe.C(\"root/${i/.C/.root}\")"
-done
--- HIJetRAA/Plots/21714_ExtractJetPlots/TranscribeAll.sh
+++
@@ -1,8 +0,0 @@
-DIR=Input20191030
-mkdir -p root
-
-for i in `ls $DIR | grep C$`
-do
-   echo Processing $i
-   root -l -b -q $DIR/${i/.C}.C "Transcribe.C(\"root/${i/.C/.root}\")"
-done

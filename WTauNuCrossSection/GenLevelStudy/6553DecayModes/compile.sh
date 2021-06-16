a28cbfa329ef5195f12157a5f49f262a424358aa
FHead <chen.yi.first@gmail.com>
2021-06-16 11:23:51 +0200

    Remove WTauNuCrossSection for submodule replacement

--- WTauNuCrossSection/GenLevelStudy/6553DecayModes/compile.sh
+++
@@ -1,5 +0,0 @@
-g++ -O2 -ansi -pedantic -W -Wall -Wshadow -fbounds-check \
-   -I/home/fhead/Programs/pythia8/pythia8145/include Taus.cpp -o TauDistributions \
-	-L/home/fhead/Programs/pythia8/pythia8145/lib/archive -lpythia8 -llhapdfdummy
-
-

4afaef8f51469a9c6dc859c3bc35f683b48666de
FHead <chen.yi.first@gmail.com>
2021-06-17 07:59:11 +0200

    fix typos

--- SwitchToGit.sh
+++ SwitchToGit.sh
@@ -2,10 +2,12 @@
 
 for i in */
 do
-   cd $i
    echo $i
    j=`echo $i | tr -d '/'`
-   git remote set-url origin git@github.com:FHead/Physics${j}.git
-   cd ..
+   (cd $i && git remote set-url origin git@github.com:FHead/Physics${j}.git)
 done
 
+# Ugh...typo...
+(cd L1TriggerUpgrade && git remote set-url origin git@github.com:FHead/PhysicsL1TriggerUpdate.git)
+(cd L1TriggerUpgrade2018 && git remote set-url origin git@github.com:FHead/PhysicsL1TriggerUpdate2018.git)
+(cd PPPrepration2017 && git remote set-url origin git@github.com:FHead/PhysicsPPPreparation2017.git)
